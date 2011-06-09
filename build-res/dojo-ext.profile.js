943cc6d8055f51750ec727abbd61124cad223311
James Dixon <jdixon@pentaho.com>
2011-06-09 19:15:51 +0000

    initial checkin of common-ui

---
+++ build-res/dojo-ext.profile.js
@@ -0,0 +1,63 @@
+    dependencies = {
+    layers:
+    [
+    //This layer is used to discard modules
+    //from the dojo package.
+
+
+
+    //This layer is used to discard modules
+    //from the dijit package.
+
+    //Custom layer mydojo.js which
+    // includes our custom Dojo artifacts
+    {
+    //place the file under dojoRootDir
+    name: "dojo-ext.js",
+    layerDependencies:
+    [
+    ],
+    dependencies:
+    [
+    "dojo",
+	"dojo.parser",
+	"dojo.cldr",
+    "dojox.storage",
+	"dojox.fx.Shadow",
+    "dijit._Widget",
+    "dijit._Templated",
+    "dijit.common",
+    "dijit.Dialog",
+    "dijit.layout.TabContainer",
+    "dijit.layout.ContentPane",
+    "dijit.layout.BorderContainer",
+    "dijit.form.Button",
+    "dijit.form.CheckBox",
+    "dijit.form.DateTextBox",
+    "dijit.form.NumberTextBox",
+    "dijit.form.TextBox",
+    "dijit.form.Select",
+    "dijit.form.MultiSelect",
+    "dijit.Toolbar",
+    "dijit.Editor",
+    "dijit.ColorPalette",
+    "dijit.ProgressBar",
+    "dijit.Tooltip",
+    "dijit.layout.TabContainer",
+    "dojo.dnd.Container",
+    "dojo.dnd.Selectors",
+	"dojo.dnd.Source",
+	"dojo.dnd.move",
+    "dojo.dnd.Avatar",
+    "dojo.dnd.Manager",
+    "dojox.widget.ColorPicker"
+    ]
+    }
+    ],
+
+    prefixes: [
+    ["dijit", "../dijit"],
+    ["dojox", "../dojox"]
+    ]
+
+    }
---
+++ build-res/pentaho-common.profile.js
@@ -0,0 +1,43 @@
+    dependencies = {
+    layers:
+    [
+    //This layer is used to discard modules
+    //from the dojo package.
+
+
+
+    //This layer is used to discard modules
+    //from the dijit package.
+
+    //Custom layer mydojo.js which
+    // includes our custom Dojo artifacts
+    {
+    //place the file under dojoRootDir
+    name: "pentaho-common.js",
+    layerDependencies:
+    [
+    ],
+    dependencies:
+    [
+    "pentaho.common.Messages",
+    "pentaho.common.state",
+    "pentaho.common.button",
+    "pentaho.common.Dialog",
+    "pentaho.common.ContextHelp",
+    "pentaho.common.MessageBox",
+    "pentaho.common.datasourceselect",
+    "pentaho.common.SplashScreen",
+    "pentaho.common.FieldList",
+    "pentaho.common.FilterDialog",
+    "pentaho.common.FilterIndicator",
+    "pentaho.common.TemplatePicker"
+    ]
+    }
+    ],
+
+    prefixes: [
+    ["dijit", "../dijit"],
+    ["dojox", "../dojox"]
+    ]
+
+    }
