83d13317de03c5c07c0eba51c069780806ba24e2
James Dixon <jdixon@pentaho.com>
2011-06-21 20:49:28 +0000

    PIR-317 - Advanced Color picker not responding appropriately mouse interactions Changed mouse coordinate calculations to handle a bug in the layerX and layerY coordinates in Safari

--- package-res/resources/web/dojo/pentaho/common/CustomColorPicker.js
+++ package-res/resources/web/dojo/pentaho/common/CustomColorPicker.js
@@ -25,7 +25,7 @@ dojo.require("dojo.i18n");
 		},
             
             _setTimer: function(mover){
-                if (!dojo.hasClass(mover.node, "dojoxHuePickerPoint")) {
+                if (!dojo.hasClass(mover.node, "dojoxHuePickerPoint") && !dojo.hasClass(mover.node, "dojoxColorPickerPoint")) {
                     // Not a dnd move event from the color picker - ignore it
                     return;
                 }                
@@ -33,12 +33,62 @@ dojo.require("dojo.i18n");
             },
 
             _clearTimer: function(mover){
-                if (!dojo.hasClass(mover.node, "dojoxHuePickerPoint")) {
+                if (!dojo.hasClass(mover.node, "dojoxHuePickerPoint") && !dojo.hasClass(mover.node, "dojoxColorPickerPoint")) {
                     // Not a dnd move event from the color picker - ignore it
                     return;
                 }                
                 this.inherited(arguments);
+            },
+
+        /* we are overriding this because the Dojo 1.6 code does not work in Safari */
+		_setPoint: function(/* Event */evt){
+			// summary: set our picker point based on relative x/y coordinates
+			//  evt.preventDefault();
+			var satSelCenterH = this.PICKER_SAT_SELECTOR_H/2;
+			var satSelCenterW = this.PICKER_SAT_SELECTOR_W/2;
+
+            var root = dojo.position(this.colorUnderlay, true);
+            var newTop = evt.pageY - root.y - satSelCenterH;
+            var newLeft = evt.pageX - root.x - satSelCenterW;
+
+			if(evt){ dijit.focus(evt.target); }
+
+			if(this.animatePoint){
+				d.fx.slideTo({
+					node: this.cursorNode,
+					duration: this.slideDuration,
+					top: newTop,
+					left: newLeft,
+					onEnd: d.hitch(this, function() {this._updateColor(true); dijit.focus(this.cursorNode);})
+				}).play();
+			}else{
+				d.style(this.cursorNode, {
+					left: newLeft + "px",
+					top: newTop + "px"
+				});
+				this._updateColor(false);
             }
+		},
+        
+        /* we are overriding this because the Dojo 1.6 code does not work in Safari */
+		_setHuePoint: function(/* Event */evt){
+			// summary: set the hue picker handle on relative y coordinates
+			var selCenter = (this.PICKER_HUE_SELECTOR_H/2);
+            var root = dojo.position(this.colorUnderlay, true);
+            var ypos = evt.pageY - root.y - selCenter;
+			if(this.animatePoint){
+				d.fx.slideTo({
+					node: this.hueCursorNode,
+					duration:this.slideDuration,
+					top: ypos,
+					left: 0,
+					onEnd: d.hitch(this, function() {this._updateColor(true); dijit.focus(this.hueCursorNode);})
+				}).play();
+			}else{
+				d.style(this.hueCursorNode, "top", ypos + "px");
+				this._updateColor(false);
+			}
+		},        
 
 	});
 })(dojo);
