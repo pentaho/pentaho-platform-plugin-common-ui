de54366f18015819ad942423f32faa1250ad0897
James Dixon <jdixon@pentaho.com>
2011-06-22 03:31:24 +0000

    PIR-320 - Drop-down component rendering issue with Safari PIR-261 - Check-marks are missing from the column content menu options for sorting and aggregation
    Created subclasses of the Dojo menu classes so we can apply themed styles to the menus

--- package-res/resources/web/dojo/pentaho/common/MenuItem.js
+++ package-res/resources/web/dojo/pentaho/common/MenuItem.js
@@ -7,14 +7,31 @@ dojo.declare(
     
         baseClass: "pentaho-menuitem",
 
-		_setSelected: function(selected){
-			// summary:
-			//		Indicate that this node is the currently selected one
-			// tags:
-			//		private
+		templateString: dojo.cache("pentaho.common", "MenuItem.html"),
 
-			dojo.toggleClass(this.domNode, "pentaho-menuitem-hover", selected);
+		_setSelected: function(selected){
+            if(!this.disabled) { 
+                dojo.toggleClass(this.domNode, "pentaho-menuitem-hover", selected);
+            }
+        },
+
+		setDisabled: function(/*Boolean*/ disabled){
+			this.set('disabled', disabled);
+			dojo.toggleClass(this.domNode, "pentaho-menuitem-disabled", disabled);
+		},
+        
+		_setDisabledAttr: function(/*Boolean*/ value){
+			// summary:
+			//		Hook for attr('disabled', ...) to work.
+			//		Enable or disable this menu item.
+
+			dijit.setWaiState(this.focusNode, 'disabled', value ? 'true' : 'false');
+			this._set("disabled", value);
+			dojo.toggleClass(this.domNode, "pentaho-menuitem-disabled", value);
 		}
+                    
+// _setStateClass: function(){
+        
         
 	}
 );
@@ -23,19 +40,16 @@ dojo.provide("pentaho.common.PopupMenuItem");
 
 dojo.declare(
     "pentaho.common.PopupMenuItem",
-    [dijit.PopupMenuItem],
+    [dijit.PopupMenuItem, pentaho.common.MenuItem],
     {
     
         baseClass: "pentaho-menuitem",
 
 		_setSelected: function(selected){
-			// summary:
-			//		Indicate that this node is the currently selected one
-			// tags:
-			//		private
-
-			dojo.toggleClass(this.domNode, "pentaho-menuitem-hover", selected);
-		}
+            if(!this.disabled) { 
+                dojo.toggleClass(this.domNode, "pentaho-menuitem-hover", selected);
+            }
+        }
         
 	}
 );
@@ -45,14 +59,29 @@ dojo.provide("pentaho.common.CheckedMenuItem");
 dojo.declare("pentaho.common.CheckedMenuItem",
     dijit.CheckedMenuItem,
     {
+
+		templateString: dojo.cache("pentaho.common", "CheckedMenuItem.html"),
+
 		_setCheckedAttr: function(/*Boolean*/ checked){
 			// summary:
 			//		Hook so attr('checked', bool) works.
 			//		Sets the class and state for the check box.
-			dojo.toggleClass(this.domNode, "pentaho-menuitem-checked", checked);
+            if(checked) {
+                dojo.addClass(this.iconNode, "pentaho-checkmenuitem-checked"); 
+                dojo.removeClass(this.iconNode, "pentaho-checkmenuitem"); 
+            } else {
+                dojo.removeClass(this.iconNode, "pentaho-checkmenuitem-checked"); 
+                dojo.addClass(this.iconNode, "pentaho-checkmenuitem"); 
+            }
 			dijit.setWaiState(this.domNode, "checked", checked);
 			this._set("checked", checked);
-		}
+		},
+
+		_setSelected: function(selected){
+            if(!this.disabled) { 
+                dojo.toggleClass(this.domNode, "pentaho-menuitem-hover", selected);
+            }
+        }
 
     }
 );
@@ -62,6 +91,22 @@ dojo.provide("pentaho.common.Menu");
 dojo.declare("pentaho.common.Menu",
 	dijit.Menu,
 	{
-        baseClass: "pentaho-menu"
+        baseClass: "pentaho-menu",
+
+		templateString: dojo.cache("pentaho.common", "Menu.html")
     }
+);
+
+
+dojo.provide("pentaho.common.MenuSeparator");
+
+dojo.declare("pentaho.common.MenuSeparator",
+		dijit.MenuSeparator,
+		{
+		// summary:
+		//		A line between two menu items
+
+		templateString: dojo.cache("pentaho.common", "MenuSeparator.html"),
+
+	}
 );
