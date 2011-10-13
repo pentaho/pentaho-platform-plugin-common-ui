c3c968625b66771474d15553ce668721b3fd0eb1
Jordan Ganoff <jganoff@pentaho.com>
2011-10-13 18:15:04 +0000

    [PIR-536] Fixed MultiButton component to allow for multiple values.

--- package-res/resources/web/prompting/pentaho-prompting-builders.js
+++ package-res/resources/web/prompting/pentaho-prompting-builders.js
@@ -245,6 +245,7 @@ pentaho.common.prompting.builders.MultiButtonBuilder = pentaho.common.prompting.
     var widget = this.base(args);
     return $.extend(widget, {
       type: 'MultiButtonComponent',
+      isMultiple: true,
       verticalOrientation: 'vertical' === args.param.attributes['parameter-layout'],
       expression: function() {
         return Dashboards.getParameterValue(this.parameter);
