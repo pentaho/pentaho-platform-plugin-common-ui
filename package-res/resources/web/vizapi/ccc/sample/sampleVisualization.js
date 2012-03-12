b08cce5b16c4cd6b6e86a1a44663aeb818c5444e
Nick Baker <nbaker@pentaho.com>
2012-03-12 19:22:15 +0000

    [BISERVER-6680] New Sample Visualization. Fixing Safari rendering issue with gem gradients

--- package-res/resources/web/vizapi/ccc/sample/sampleVisualization.js
+++ package-res/resources/web/vizapi/ccc/sample/sampleVisualization.js
@@ -1,18 +1,11 @@
 pentaho.visualizations.push({
-  id: 'sample_bar',                          // unique identifier
-  type: 'barchart',                       // generic type id
-  source: 'CCC',                          // id of the source library
-  name: 'CCC Column',                     // visible name, this will come from a properties file eventually
-  'class': 'pentaho.ccc.CccChart',          // type of the Javascript object to instantiate
+  id: 'sample_calc',                          // unique identifier
+  type: 'calc',                       // generic type id
+  source: 'Sample',                          // id of the source library
+  name: 'Sample Calculation',                     // visible name, this will come from a properties file eventually
+  'class': 'pentaho.sample.Calc',          // type of the Javascript object to instantiate
   args: {                                 // arguments to provide to the Javascript object
-    cccClass: 'pvc.BarChart',
-    stacked: false,
-    orientation: 'vertical',
-    extensionPoints: {
-      xAxisLabel_textAngle: -1,
-      xAxisLabel_textAlign: "right",
-      xAxisLabel_textBaseline: "top"
-    }
+
   },
   propMap: [],
   dataReqs: [                             // dataReqs describes the data requirements of this visualization
@@ -23,17 +16,93 @@ pentaho.visualizations.push({
             {   id: 'rows',             // id of the data element
               dataType: 'string',         // data type - 'string', 'number', 'date', 'boolean', 'any' or a comma separated list
               dataStructure: 'column',    // 'column' or 'row' - only 'column' supported so far
-              caption: 'Category',        // visible name
-              required: true              // true or false
+              caption: 'Level',        // visible name
+              required: true,              // true or false
+              allowMultiple: false,
+              ui: {
+                group: "data"
+              }
             },
             {   id: 'measures',
               dataType: 'number',
               dataStructure: 'column',
-              caption: 'Values',
+              caption: 'Measure',
               required: true,
-              allowMultiple: true         // true or false
+              allowMultiple: false,
+              ui: {
+                group: "data"
+              }
+            },
+            {
+              id: 'calc',
+              dataType: 'string',
+              values: ["MIN", "MAX", "AVG"],
+              ui: {
+                labels: ["Minimum", "Maximum", "Average"],
+                group: "options",
+                type: 'combo',
+                caption: "Calculation"
+              }
+            },
+            {
+              id: 'fontSize',
+              dataType: 'string',
+              ui: {
+                labels: ["Minimum", "Maximum", "Average"],
+                group: "options",
+                type: 'combo',
+                caption: "Calculation"
+              }
             }
           ]
     }
   ]
 });
+
+pentaho.sample = {}
+
+pentaho.sample.Calc = function(canvasElement){
+  this.canvasElement = canvasElement;
+  this.numSpan = document.createElement("span");
+  this.numSpan.style.fontSize = "42px";
+  this.numSpan.style.position = "relative";
+  this.canvasElement.appendChild(this.numSpan);
+
+}
+pentaho.sample.Calc.prototype.resize = function(width, height){
+  this.numSpan.style.left = ((this.canvasElement.offsetWidth - this.numSpan.offsetWidth) / 2) + "px"
+  this.numSpan.style.top = ((this.canvasElement.offsetHeight - this.numSpan.offsetHeight) / 2) + "px"
+};
+
+pentaho.sample.Calc.prototype.draw = function(datView, vizOptions) {
+  var rows = datView.dataTable.jsonTable.rows;
+  var dataArray = [];
+  for(var i=0; i<rows.length; i++){
+    dataArray.push(rows[i].c[1].v);
+  }
+  var value = 0;
+  switch(vizOptions.calc){
+    case "MAX":
+      for(var i=0; i< dataArray.length; i++){
+        value = Math.max(value, dataArray[i]);
+      }
+      break;
+    case "MIN":
+      for(var i=0; i< dataArray.length; i++){
+        value = Math.min(value, dataArray[i]);
+      }
+      break;
+    case "AVG":
+      var total = 0;
+      for(var i=0; i< dataArray.length; i++){
+        total += dataArray[i];
+      }
+      value = total / dataArray.length;
+      break;
+    default:
+
+  }
+
+  this.numSpan.innerHTML = value;
+  this.resize();
+}
--- package-res/resources/web/vizapi/ccc/sample/sampleVisualization_analyzer_plugin.js
+++ package-res/resources/web/vizapi/ccc/sample/sampleVisualization_analyzer_plugin.js
@@ -2,32 +2,62 @@ var analyzerPlugins = analyzerPlugins || [];
 analyzerPlugins.push(
     {
       init:function () {
-        // Register types to display in Analyzer
-        cv.pentahoVisualizations.push(pentaho.visualizations.getById("sample_bar"));
 
-        cv.pentahoVisualizationHelpers["sample_bar"] = {
+        // Register visualizations to display in Analyzer
+        cv.pentahoVisualizations.push(pentaho.visualizations.getById("sample_calc"));
 
+
+
+        /* 
+         Helpers contain code that knows about the Analyzer specific context. The one function that's required
+         "generateOptionsFromAnalyzerState" is called so the visualization can set it's own options based on
+         Analyzers current report.
+         */
+        cv.pentahoVisualizationHelpers["sample_calc"] = {
           // set visualization options based on analyzer's state.
-          generateOptionsFromAnalyzerState:function (report) { 
+          generateOptionsFromAnalyzerState:function (report) {
             return {}; // perform no work
           }
-
         };
-        
-        dojo.declare("SampleBarConfig", [analyzer.LayoutConfig], {
+
+        /*
+         LayoutConfig objects manage the interaction between Analyzer's Layout Panel and the visualization's settings.
+         */
+
+        // Declare a new class which extends the built-in version from Analyzer
+        dojo.declare("SampleConfig", [analyzer.LayoutConfig], {
 
 
-          onModelEvent:function (config, item, eventName, args) {
+          /**
+           * @param config    The parse Configuration object which serves as the model of of the Panel
+           * @param item      The item in the panel which originated the event.
+           * @param eventName The name of the event (clicked, value, etc)
+           * @param args      A Hash Object containing relevent values (prevVal, newVal, etc)
+           */
+          onModelEvent: function(config, item, eventName, args) {
+            this.report.visualization.args["calc"] = config.byId("calc").value;
             this.inherited(arguments); // Let super class handle the insertAt and removedGem events
           },
-          getConfiguration:function () {
+
+          /**
+           * Return the JSON configuration object which the panel will use to create the UI and it's model
+           */
+          getConfiguration: function() {
             var config = this.inherited(arguments);
-            // make customizations to the standard config here, set current values, toggle things on/off
+
+            dojo.forEach(config.properties, function (item) {
+              if (this.report.visualization.args[item.id] !== "undefined") {
+                item.value = this.report.visualization.args[item.id];
+              }
+            }, this);
+
             return config;
 
           }
         });
-        analyzer.LayoutPanel.configurationManagers["JSON_sample_bar"] = SampleBarConfig;
+
+        // Register the Layout Panel Configuration Manager
+        analyzer.LayoutPanel.configurationManagers["JSON_sample_calc"] = SampleConfig;
+      }
     }
-  }
 );
