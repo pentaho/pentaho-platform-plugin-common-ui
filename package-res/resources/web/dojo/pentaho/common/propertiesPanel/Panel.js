f7492b9ae4a4cea85b88a4b74f4e11e56363f34e
Rob Fellows <rfellows@pentaho.com>
2012-03-13 15:07:08 +0000

    ANALYZER-1040 - In IE, the 'reverse colors' check box does not work (geo map and heat grid chart option)
    Putting back in changes for a fix in that got stomped on.

--- package-res/resources/web/dojo/pentaho/common/propertiesPanel/Panel.js
+++ package-res/resources/web/dojo/pentaho/common/propertiesPanel/Panel.js
@@ -917,7 +917,7 @@ dojo.declare(
       className: "propPanel_checkbox",
       widgetsInTemplate: true,
       value : false,
-      templateString: "<div class='${className}'><input id='${model.id}_checkbox' name='${model.id}_checkbox' dojoType='dijit.form.CheckBox' checked='${value}'/> <label for='${model.id}_checkbox'>${label}</label></div>",
+      templateString: "<div class='${className}'><input id='${model.id}_checkbox' name='${model.id}_checkbox' dojoType='dijit.form.CheckBox' /> <label for='${model.id}_checkbox'>${model.ui.label}</label></div>",
       constructor:function (options) {
         if(typeof(this.model.value) !== "undefined"){
           this.value = this.model.value;
@@ -927,11 +927,16 @@ dojo.declare(
       postCreate: function(){
         this.checkbox = dijit.byId(this.model.id+"_checkbox");
         var outterThis = this;
+
+        // ANALYZER-1040, IE checkbox issues force us to set the checked status here rather than in the templateString
+        if(typeof(this.value) != 'boolean') {
+          this.value = false;
+        }
+        this.checkbox.attr('checked', this.value);
+
         this.connect(this.checkbox, "onChange", function(){
           outterThis.model.set('value', outterThis.checkbox.checked);
         });
-
-        this.inherited(arguments);
       },
       set: function(prop, newVal){
         if(this.checkbox){
