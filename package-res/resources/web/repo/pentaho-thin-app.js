f3c12552b2e742f8083edb9ce42954ca8b4cddf8
Rowell Belen <rbelen@pentaho.com>
2012-03-20 21:16:56 +0000

    [PPP-2848] Added an abstraction to the collapse browser mantle command.

--- package-res/resources/web/repo/pentaho-thin-app.js
+++ package-res/resources/web/repo/pentaho-thin-app.js
@@ -127,6 +127,11 @@ this.refreshBrowsePanel = function() {
     }
 }
 
+this.collapseBrowsePanel = function() {
+	if( this.console_enabled){
+        window.parent.executeCommand("CollapseBrowserCommand");
+	}
+}
   
 }
 
