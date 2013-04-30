f399cc3b23acb92ae69e436ca0f600403f55b72b
Nick Baker <nbaker@pentaho.com>
2013-04-30 13:36:57 -0400

    [BISERVER-8627] Making the home perspective theme-able. Also externalizing common javascript to common-ui plugin.

--- package-res/resources/web/common-ui-require-js-cfg.js
+++ package-res/resources/web/common-ui-require-js-cfg.js
@@ -5,19 +5,19 @@ requireCfg['paths']['common-repo'] = CONTEXT_PATH+'api/repos/common-ui/resources
 requireCfg['paths']['common-data'] = CONTEXT_PATH+'api/repos/common-ui/resources/web/dataapi';
 
 requireCfg['shim']['common-repo/module'] = [
-	'common-repo/state',
-	'common-repo/pentaho-ajax',
-	'common-repo/pentaho-thin-app'
+  'common-repo/state',
+  'common-repo/pentaho-ajax',
+  'common-repo/pentaho-thin-app'
 ];
 
 requireCfg['shim']['common-data/module'] = [
-	'common-repo/module',
-	'common-data/oop',
-	'common-data/app',
-	'common-data/controller',
-	'common-data/xhr',    
-	'common-data/cda',
-	'common-data/models-mql'
+  'common-repo/module',
+  'common-data/oop',
+  'common-data/app',
+  'common-data/controller',
+  'common-data/xhr',
+  'common-data/cda',
+  'common-data/models-mql'
 ];
 
 requireCfg['shim']['common-data/app'] = ['common-data/oop'];
@@ -25,3 +25,14 @@ requireCfg['shim']['common-data/controller'] = ['common-data/oop', 'common-data/
 requireCfg['shim']['common-data/xhr'] = ['common-data/oop'];
 requireCfg['shim']['common-data/cda'] = ['common-data/oop'];
 requireCfg['shim']['common-data/models-mql'] = ['common-data/oop', 'common-data/controller'];
+
+
+requireCfg['paths']['common-ui/jquery'] = CONTEXT_PATH+'api/repos/common-ui/resources/web/jquery/jquery-1.9.1.min';
+requireCfg['paths']['common-ui/handlebars'] = CONTEXT_PATH+'api/repos/common-ui/resources/web/handlebars/handlebars';
+requireCfg['paths']['common-ui/jquery-i18n'] = CONTEXT_PATH+'api/repos/common-ui/resources/web/jquery/jquery.i18n.properties-min';
+requireCfg['paths']['common-ui/bootstrap'] = CONTEXT_PATH+'api/repos/common-ui/resources/web/bootstrap/bootstrap.min';
+requireCfg['paths']['common-ui/bootstrap-widgets'] = CONTEXT_PATH+'api/repos/common-ui/resources/web/bootstrap/widgets';
+
+requireCfg['shim']['common-ui/bootstrap'] = ['common-ui/jquery', 'common-ui/bootstrap-widgets'];
+requireCfg['shim']['common-ui/jquery-i18n'] = ['common-ui/jquery'];
+requireCfg['shim']['common-ui/handlebars'] = ['common-ui/jquery'];
