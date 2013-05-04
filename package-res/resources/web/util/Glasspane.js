0937f7ce3860491b8a582590fc38d05122c7dda7
Rob Fellows <rfellows@pentaho.com>
2013-05-04 01:03:44 -0400

    BISERVER-8967 - updated common spinner and glasspane, tied them into data-access.

--- package-res/resources/web/util/Glasspane.js
+++ package-res/resources/web/util/Glasspane.js
@@ -1,41 +1,49 @@
 pen.define([], function() {
 
-  return {
-    id: "pentaho-glasspane-overlay",
-    fadeInDuration: 200,
+  var gp = {
+    fadeInDuration: 0,
     fadeOutDuration: 500,
+    $glasspane: undefined,
 
     /**
      * overlay a translucent panel across the entire window
      */
-    show: function() {
-      var widget = this.getContainer();
-
-      // only create the DOM element if it's not already there
-      if(!widget.get(0)) {
-        // be sure to style it with the "glasspane" style found in the pentaho-themes
-        var waitHtml = "<div id='" + this.id + "' class='glasspane'></div>";
-        $(window.top.document.body).append(waitHtml);
-        $(window.top.document).find("#" + this.id).fadeIn(this.fadeInDuration);
-      } else {
-        // it's already available to use, just show it
-        this.getContainer().fadeIn(this.fadeInDuration);
+    show: function(/*Optional|Integer*/ zIndex) {
+      // notify glasspane listeners that we are showing an glasspane
+      if(window.top.mantle_notifyGlasspaneListeners) {
+        window.top.mantle_notifyGlasspaneListeners(true);
       }
 
+      this.$glasspane = $("<div class='glasspane'></div>");
+      if(zIndex) {
+        this.$glasspane.css({'z-index': '' + zIndex});
+      }
+      $(window.top.document.body).append(this.$glasspane);
+      this.$glasspane.fadeIn(this.fadeInDuration, function() {
+        // nothing to do after it fades in...
+      });
     },
 
     /**
      * hide it
      */
     hide: function() {
-      this.getContainer().fadeOut(this.fadeOutDuration);
-    },
-
-    getContainer: function() {
-      // no matter where this is called from, it goes all the way to the top
-      return $(window.top.document).find("#" + this.id);
+      // notify glasspane listeners that we are hiding a glasspane
+      if(window.top.mantle_notifyGlasspaneListeners) {
+        window.top.mantle_notifyGlasspaneListeners(false);
+      }
+      var that = this;
+      this.$glasspane.fadeOut(this.fadeOutDuration, function() {
+        if(that.$glasspane) {
+          that.$glasspane.remove();
+        }
+      });
     }
 
   };
+
+  var glasspane = function() {};
+  glasspane.prototype = gp;
+  return glasspane;
 
 });
