7ee31e5b01a5b87b6e5e76e9b644ce1561449cac
mkambol <mkambol@gmail.com>
2013-08-15 11:13:50 -0400

    [PRD-3882]  Adding source attribution and licensing for the base64/utf8 decoding functions.

--- package-res/resources/web/util/base64.js
+++ package-res/resources/web/util/base64.js
@@ -1,6 +1,20 @@
 /**
+ * The methods below are adapted from jQuery SDK.
+ *
+ * http://www.jquerysdk.com/docs/Downloads#jQuery_SDK_1.4
+ *
+ * @author			Emanuel Vitzthum
+ * @copyright		© 2012 jQuery SDK v1.4
+ * @info			http://www.jquerysdk.com
+ *
+ * @license			Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) and GPL (http://www.gnu.org/licenses/gpl.html)
+ *
+ */
+
+/**
  * Cross-platform base64 decoding.  btoa() is not supported on IE, and
- * doesn't handle UTF8.
+ * doesn't handle UTF8
+ *
  */
 function base64Decode(encoded) {
   var indexTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
