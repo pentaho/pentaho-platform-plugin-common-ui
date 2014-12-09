dc5e83776daf9d1da99a9a03ad58424b9eb2db3d
Kris Rivera <krivera@pentaho.com>
2014-12-09 15:17:06 -0500

    [BACKLOG-1222] -- updated karma unit test to match change in functionality

--- package-res/resources/web/test/karma/unit/pluginSpec.js
+++ package-res/resources/web/test/karma/unit/pluginSpec.js
@@ -1,58 +1,62 @@
 var deps = [
-    "common-ui/Plugin",
-    "common-ui/PluginHandler"
+  "common-ui/Plugin",
+  "common-ui/PluginHandler"
 ];
 
 require(deps, function(PentahoPlugin, PentahoPluginHandler) {
-	
-	describe("Pentaho Plugin", function(){
-		
-		var plugin;
-		var pluginHandler;
-		
-		describe("Constructor Tests", function() {
-			it("should fail when creating a plugin without a plugin handler", function() {
-				expect(function(){ 
-					new PentahoPlugin({}); 
-				}).toThrow(PentahoPlugin.errMsgs.noPluginHandler);
-            })
 
-            it("should fail when creating a plugin and assigning an object that is not a plugin handler", function() {
-            	expect(function(){ 
-            		new PentahoPlugin({ pluginHandler : new Object() }); 
-            	}).toThrow(PentahoPlugin.errMsgs.notAPluginHandler);
-            })
-		});
+  describe("Pentaho Plugin", function() {
 
-		describe("Function Tests", function() {
-			beforeEach(function() {
-				pluginHandler = new PentahoPluginHandler();
-				
-				plugin = new PentahoPlugin({
-					pluginHandler : pluginHandler,
-					onRegister : function() {},
-					onUnregister : function() {}
-				});
-				
-				spyOn(plugin, "onRegister");
-				spyOn(plugin, "onUnregister");
-			});
-			
-			it("should register a plugin", function() {
-				plugin.register();
-				
-				expect(pluginHandler.get(plugin.id)).toEqual(plugin);
-				expect(plugin.onRegister).toHaveBeenCalled();
-			});
-			
-			it("should register, then unregister a plugin", function() {
-				plugin.register();
-				plugin.unregister();
-				
-				expect(pluginHandler.get(plugin.id)).not.toBeDefined();
-				expect(plugin.onUnregister).toHaveBeenCalled();
-			})
-		})
-		
-	});
+    var plugin;
+    var pluginHandler;
+
+    describe("Constructor Tests", function() {
+      it("should fail when registering a plugin without a plugin handler", function() {
+        var plugin = new PentahoPlugin({});
+        expect(function() {
+          plugin.register();
+        }).toThrow(PentahoPlugin.errMsgs.noPluginHandler);
+      });
+
+      it("should fail when creating a plugin and assigning an object that is not a plugin handler", function() {
+        var plugin = new PentahoPlugin({pluginHandler: new Object()});
+        expect(function() {
+          plugin.register();
+        }).toThrow(PentahoPlugin.errMsgs.notAPluginHandler);
+      })
+    });
+
+    describe("Function Tests", function() {
+      beforeEach(function() {
+        pluginHandler = new PentahoPluginHandler();
+
+        plugin = new PentahoPlugin({
+          pluginHandler: pluginHandler,
+          onRegister: function() {
+          },
+          onUnregister: function() {
+          }
+        });
+
+        spyOn(plugin, "onRegister");
+        spyOn(plugin, "onUnregister");
+      });
+
+      it("should register a plugin", function() {
+        plugin.register();
+
+        expect(pluginHandler.get(plugin.id)).toEqual(plugin);
+        expect(plugin.onRegister).toHaveBeenCalled();
+      });
+
+      it("should register, then unregister a plugin", function() {
+        plugin.register();
+        plugin.unregister();
+
+        expect(pluginHandler.get(plugin.id)).not.toBeDefined();
+        expect(plugin.onUnregister).toHaveBeenCalled();
+      })
+    })
+
+  });
 })
