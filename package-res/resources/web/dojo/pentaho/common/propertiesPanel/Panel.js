pentaho/pentaho-platform-plugin-common-ui

commit 8afda78651cbdffe677a14923daceb81ac0973b2

[ANALYZER-3058] - Backport of ANALYZER-3058 - Memory leaks during multiple changing of chart type * Implemented a ccc wrapper dispose method that disposes the underlying CCC chart, if any, and clears DOM references * Implemented a VizController dispose method that removes events from it and the viz, if any, and clears DOM references * Replaced the VizAPI Events implementation by a non-leaky one that stores event listeners directly on the emitting objects    * Was leaking every controller, chart... * Panel    * .subscriptions and .handles on the prototype... were leaking... a lot    * detaching from DOM events    * calling destroys that weren't being called    * cleared lots of dom references    * fixed this.handles.push[on(...)] ...

package-res/resources/web/dojo/pentaho/common/propertiesPanel/Panel.js
@@ -248,6 +248,10 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
               this.propUIs.forEach(function (widget) {
                 widget.destroyRecursive();
               });
+              for(var gid in this.groups) {
+                this.groups[gid].destroyRecursive();
+              }
+
               this.propUIs = [];
               this.groups = {};
               this.domNode.innerHTML = "";
@@ -279,16 +283,27 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
               var outterThis = this;
               this.model.watch(function (propName, prevVal, newVal) {
 
+              this._watchHandle = this.model.watch(function(propName, prevVal, newVal) {
                 switch (propName) {
                   case "value":
                   case "default":
-                    outterThis.set(propName, newVal);
+                    if(!outterThis._destroyed) outterThis.set(propName, newVal);
                     break;
                 }
               });
             },
             onUIEvent: function (type, args) {
+            },
 
+            destroy: function() {
+              this.inherited(arguments);
+
+              this.model = this.propPanel = null;
+
+              if(this._watchHandle) {
+                this._watchHandle.remove();
+                this._watchHandle = null;
+              }
             }
           }
       );
@@ -334,6 +349,11 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
             }
 
           }
+        },
+
+        destroy: function() {
+          this.inherited(arguments);
+          this.dropIndicator = this.gemUIbeingInserted = this.gemBar = null;
         },
 
         _redirectMouseUp: function (e) {
@@ -585,6 +605,11 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
           var ok = this.dropZone.checkAcceptance(source, nodes, silent);
           ;
           return ok;
+        },
+
+        destroy: function() {
+          this.inherited(arguments);
+          this.dropZone = null;
         }
       });
 
@@ -603,6 +628,10 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
             constructor: function (options) {
               this.id = this.model.id + "_ui";
               this.showPlaceholder = this.model.ui.showPlaceholder;
+
+              this.handles = [];
+              this.subscriptions = [];
+
               if (this.model.ui.placeholderText) {
                 this.placeholderText = this.model.ui.placeholderText;
               }
@@ -623,7 +652,7 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
               // new pentaho.common.propertiesPanel.PlaceholderSource(this.domNode, {accept: this.model.ui.dndType, dropZone: this.dropZone});
 
               if (this.showPlaceholder && (this.model.allowMultiple || this.model.gems.length < 2)) {
-                new PlaceholderSource(this.placeholder, {accept: this.model.ui.dndType, dropZone: this.dropZone});
+                this._placeHolderSource = new PlaceholderSource(this.placeholder, {accept: this.model.ui.dndType, dropZone: this.dropZone});
 
 
                 // dojo.connect(this.placeholder.firstChild, "onmouseover", function(event){
@@ -652,29 +681,27 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
               this.subscriptions.push(topic.subscribe("/dnd/cancel", unSubscribeFunc));
               this.subscriptions.push(topic.subscribe("/dnd/drop", unSubscribeFunc));
 
-
-              on(this.domNode,  "mouseover", function (event) {
+              this.handles.push(on(this.domNode,  "mouseover", function (event) {
                 if (ManagerClass.manager().source && outterThis.checkAcceptance(outterThis.dropZone,  ManagerClass.manager().nodes)) {
-                  domClass.add(outterThis.domNode, "over");
+                  outterThis._showOver();
                 }
-              });
-              on(this.domNode, "mouseout", function (event) {
-                domClass.remove(outterThis.domNode,  "over");
-              });
-              on(this.domNode, "mouseup", function (event) {
-                domClass.remove(outterThis.domNode,  "over");
-              });
+              }));
+              this.handles.push(on(this.domNode, "mouseout", function (event) {
+                outterThis._hideOver();
+              }));
+              this.handles.push(on(this.domNode, "mouseup", function (event) {
+                outterThis._hideOver();
+              }));
 
-
-              // this.handles.push[on(this.dropZone,  "onDrop", lang.hitch( this,  "onDrop"))];
-              this.handles.push[on(this.dropZone,  "createDropIndicator", lang.hitch( this,  "createDropIndicator"))];
-              this.handles.push[on(this.dropZone,  "placeDropIndicator", lang.hitch( this,  "placeDropIndicator"))];
-              this.handles.push[on(this.dropZone,  "onMouseOver", lang.hitch( this,  "onMouseOver"))];
-              this.handles.push[on(this.dropZone,  "onMouseOut", lang.hitch( this,  "onMouseOut"))];
-              this.handles.push[on(this.dropZone,  "onDraggingOver", lang.hitch( this,  "onDraggingOver"))];
-              this.handles.push[on(this.dropZone,  "onDraggingOver", lang.hitch( this,  "onDraggingOut"))];
-              // this.handles.push[on(this.dropZone,  "checkAcceptance", lang.hitch( this,  "checkAcceptance"))];
-              this.handles.push[on(this.dropZone,  "insertNodes", lang.hitch( this,  "insertNodes"))];
+              // this.handles.push(on(this.dropZone,  "onDrop", lang.hitch( this,  "onDrop")));
+              this.handles.push(on(this.dropZone,  "createDropIndicator", lang.hitch( this,  "createDropIndicator")));
+              this.handles.push(on(this.dropZone,  "placeDropIndicator", lang.hitch( this,  "placeDropIndicator")));
+              this.handles.push(on(this.dropZone,  "onMouseOver", lang.hitch( this,  "onMouseOver")));
+              this.handles.push(on(this.dropZone,  "onMouseOut", lang.hitch( this,  "onMouseOut")));
+              this.handles.push(on(this.dropZone,  "onDraggingOver", lang.hitch( this,  "onDraggingOver")));
+              this.handles.push(on(this.dropZone,  "onDraggingOver", lang.hitch( this,  "onDraggingOut")));
+              // this.handles.push(on(this.dropZone,  "checkAcceptance", lang.hitch( this,  "checkAcceptance")));
+              this.handles.push(on(this.dropZone,  "insertNodes", lang.hitch( this,  "insertNodes")));
 
               array.forEach(this.model.gems, function (gem) {
                 var uiClass = Panel.registeredTypes["gem"];
@@ -791,14 +818,36 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
               this.inherited(arguments);
               // destroyRecursive should do this, investigate
               array.forEach(this.gems, function (gem) {
-                gem.destroy();
+                gem.destroyRecursive();
               });
-              this.destroy();
-              array.forEach(this.handles, function(handle){handle.remove()});
             },
             destroy: function () {
+              if(this.handles) {
+              array.forEach(this.handles, function(handle){handle.remove()});
+                this.handles = null;
+              }
+              if(this.subscriptions) {
               array.forEach(this.subscriptions, function(handle){handle.remove()});
+                this.subscriptions = null;
+              }
+
+              this.dropZoneNode = null;
+              if(this.dropZone) {
+                this.dropZone.destroy();
+                this.dropZone = null;
+              }
+
+              this.placeholder = null;
+              if(this._placeHolderSource) {
+                this._placeHolderSource.destroy();
+                this._placeHolderSource = null;
+              }
+
               this.inherited(arguments);
+
+              // Prevent leak
+              this._startupWidgets = null;
+              this._supportingWidgets = null;
             }
           }
       );
@@ -814,32 +863,48 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
               this.gemBar = options.gemBar;
               this.dndType = options.dndType;
               this.id = options.id;
+              this.handles = [];
             },
             detach: function () {
               model.detach();
             },
+            destroy: function() {
+              this.inherited(arguments);
+              if(this.handles) {
+                array.forEach(this.handles, function(handle){handle.remove()});
+                this.handles = null;
+              }
+              this.menuHandle = null;
+              this.postDrop   = null;
+              this.gemBar     = null;
+
+              // Prevent leak
+              this._startupWidgets = null;
+              this._supportingWidgets = null;
+            },
             postCreate: function () {
-              on(this.domNode, "contextmenu", lang.hitch( this,  "onContextMenu"));
+              this.handles.push(on(this.domNode, "contextmenu", lang.hitch( this,  "onContextMenu")));
+
               var outterThis = this;
               this.menuHandle = query("div.gemMenuHandle", this.domNode)[0];
 
               var gemLabel = query("div.gem-label", this.domNode)[0];
               gemLabel.appendChild(document.createTextNode(this.model.value));
 
-              on(query("div.gemMenuHandle",  this.domNode)[0], "mouseover",  function (e) {
+              this.handles.push(on(query("div.gemMenuHandle",  this.domNode)[0], "mouseover",  function (e) {
                 if (!ManagerClass.manager().source) {
                   domClass.add(e.target, "over");
                 }
-              });
-              on(query("div.gemMenuHandle", this.domNode)[0], "mouseout", function (e) {
+              }));
+              this.handles.push(on(query("div.gemMenuHandle", this.domNode)[0], "mouseout", function (e) {
                 if (!ManagerClass.manager().source) {
                   domClass.remove(e.target, "over");
                 }
-              });
-              on(this.menuHandle, "click", lang.hitch( this,  "onContextMenu"));
+              }));
+              this.handles.push(on(this.menuHandle, "click", lang.hitch( this,  "onContextMenu")));
 
-              on(this.domNode, "mouseover", lang.hitch( this,  "onMouseOver"));
-              on(this.domNode, "mouseout", lang.hitch( this,  "onMouseOut"));
+              this.handles.push(on(this.domNode, "mouseover", lang.hitch( this,  "onMouseOver")));
+              this.handles.push(on(this.domNode, "mouseout", lang.hitch( this,  "onMouseOut")));
               this.inherited(arguments);
             },
             onMouseOver: function () {
@@ -872,7 +937,7 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
             constructor: function (options) {
               this.name = options.id;
               this.options = [];
-
+              this.handles = [];
               array.forEach(this.model.values, function (val, idx) {
                 var opt = {label: val, value: val};
                 if (this.model.ui.labels) {
@@ -928,7 +993,7 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
                 // use the styled drop down
 
                 domClass.add(this.domNode, this.className);
-                var sel = new Select({
+                var sel = this.selNode = new Select({
                   options: opts,
                   onChange: function () {
                     me.model.set('value', this.value);
@@ -951,6 +1016,14 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
             destroy: function () {
               array.forEach(this.handles, function(handle){handle.remove()});
               this.inherited(arguments);
+              if(this.selNode) {
+                this.selNode.destroyRecursive();
+                this.selNode = null;
+              }
+
+              // Prevent leak
+              this._startupWidgets = null;
+              this._supportingWidgets = null;
             }
 
           }
@@ -1031,6 +1104,15 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
             destroy: function () {
               array.forEach(this.handles, function(handle){handle});
               this.inherited(arguments);
+
+              if(this.checkbox) {
+                this.checkbox.destroyRecursive();
+                this.checkbox = null;
+              }
+
+              // Prevent leak
+              this._startupWidgets = null;
+              this._supportingWidgets = null;
             }
           }
       );
@@ -1054,6 +1136,14 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
 
             onClick: function () {
               this.model.set('clicked', true);
+            },
+
+            destroy: function() {
+              this.inherited(arguments);
+
+              // Prevent leak
+              this._startupWidgets = null;
+              this._supportingWidgets = null;
             }
           }
       );

package-res/resources/web/vizapi/Events.js
@@ -28,75 +28,61 @@ pentaho = typeof pentaho == "undefined" ? {} : pentaho;
 
 pentaho.events = {};
 
-// an array of event listeners
-pentaho.events.listeners = [];
-
 /*
     trigger
     Triggers an event by notifying all of the listeners that the event has occurred.
 */
-pentaho.events.trigger = function( object, eventName, args ) {
-
-    // examine each listener
-    for(var lNo=0; lNo<pentaho.events.listeners.length; lNo++ ) {
-        if( pentaho.events.listeners[lNo].object == object && pentaho.events.listeners[lNo].eventName == eventName ) {
-            // the event matches this listener's object and event name
-            pentaho.events.listeners[lNo].func( args );
+pentaho.events.trigger = function(object, eventName, arg) {
+    var events = object && object.__events__;
+    if(events) {
+        handlers = events[eventName];
+        if(handlers) {
+            var i = -1, L = handlers.length;
+            while(++i < L) handlers[i](arg);
         }
     }
-}
+};
 
 /*
     addListener
     Adds a listener for an event
-    
+
     object      The object to listen to
     eventName   The name of the event to listen to
     func        The function to call when the event happens
 */
-pentaho.events.addListener = function(object, eventName, func ) {
-    pentaho.events.listeners.push({
-        object: object,
-        eventName: eventName,
-        func: func
-    });
-}
+pentaho.events.addListener = function(object, eventName, func) {
+    if(object) {
+        var events   = (object.__events__ || (object.__events__ = {})),
+            handlers = (events[eventName] || (events[eventName] = []));
+        handlers.push(func);
+    }
+};
 
 /*
     removeListener
     Removes a listener for an event
-    
+
     object      The object to listen to
     eventName   The name of the event to listen to
 */
-pentaho.events.removeListener = function(object, eventName ) {
-
-    var lNo = 0;
-    while(lNo<pentaho.events.listeners.length ) {
-        if( pentaho.events.listeners[lNo].object == object && pentaho.events.listeners[lNo].eventName == eventName ) {
-            pentaho.events.listeners.splice(lNo,1);
+pentaho.events.removeListener = function(object, eventName) {
+    var events = object && object.__events__;
+    if(events) {
+        if(eventName) {
+            delete events[eventName];
         } else {
-            lNo++;
+            delete object.__events__;
         }
     }
-    
-}
+};
 
 /*
     removeSource
-    Removes all the listeners for the specified ibject
-    
+    Removes all the listeners for the specified object
+
     object      The object to listen to remove
 */
 pentaho.events.removeSource = function(object) {
-
-    var lNo = 0;
-    while(lNo<pentaho.events.listeners.length ) {
-        if( pentaho.events.listeners[lNo].object == object ) {
-            pentaho.events.listeners.splice(lNo,1);
-        } else {
-            lNo++;
-        }
-    }
-    
-}
+    pentaho.events.removeListener(object);
+};

package-res/resources/web/vizapi/VizController.js
@@ -133,6 +133,17 @@ pentaho.VizController = function(id) {
   this.formatInfo = null;
 }
 
+pentaho.VizController.prototype.dispose = function() {
+  pentaho.events.removeSource(this);
+  if(this.chart) {
+    if(this.chart.dispose) this.chart.dispose();
+    pentaho.events.removeSource(this.chart);
+    this.chart = null;
+  }
+
+  this.domNode = this.visualPanelElement = null;
+};
+
 /*
  getError
  Returns the most recent Javascript error object

package-res/resources/web/vizapi/ccc/ccc_wrapper.js
@@ -1657,6 +1657,14 @@ function(def, pvc, pv){
 
         /* PLUGIN INTERFACE  */
 
+        dispose: function() {
+           this._element = null;
+           if(this._chart && this._chart.dispose) {
+            this._chart.dispose();
+            this._chart = null;
+           }
+        },
+
         /**
          * Instructs the visualization to draw itself with
          * supplied data and options.

commit 61b650e9923aeec1debf9faf277fe77610ac8f93

[ANALYZER-3058] - Backport of ANALYZER-3058 - Memory leaks during multiple changing of chart type (5.4 Suite) * fix a syntax error introduced in 8afda78651cbdffe677a14923daceb81ac0973b2

package-res/resources/web/dojo/pentaho/common/propertiesPanel/Panel.js
@@ -281,8 +281,6 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
               this.model = options.model;
               this.propPanel = options.propPanel;
               var outterThis = this;
-              this.model.watch(function (propName, prevVal, newVal) {
-
               this._watchHandle = this.model.watch(function(propName, prevVal, newVal) {
                 switch (propName) {
                   case "value":