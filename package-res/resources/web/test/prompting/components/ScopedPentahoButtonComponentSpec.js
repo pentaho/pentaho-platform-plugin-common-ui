0d6a14168f11a1d43d002d4d0a4dc673862f15e1
Anna Popova1 <Anna_Popova1@epam.com>
2015-07-09 17:29:42 +0400

    [BACKLOG-4062] Last part of tests for prompt components

---
+++ package-res/resources/web/test/prompting/components/ScopedPentahoButtonComponentSpec.js
@@ -0,0 +1,78 @@
+/*!
+ * Copyright 2010 - 2015 Pentaho Corporation.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+define([ 'common-ui/prompting/components/ScopedPentahoButtonComponent', 'cdf/lib/jquery' ], function(
+  ScopedPentahoButtonComponent, $) {
+
+  describe("ScopedPentahoButtonComponent", function() {
+    it("should create component", function() {
+      var comp = new ScopedPentahoButtonComponent();
+      expect(comp.viewReportButtonRegistered).toBeFalsy();
+    });
+
+    describe("update", function() {
+      it("should register submit event", function() {
+        var comp = new ScopedPentahoButtonComponent();
+        spyOn(comp, "registerSubmitClickEvent");
+        comp.update();
+        expect(comp.registerSubmitClickEvent).toHaveBeenCalled();
+      });
+    });
+
+    describe("registerSubmitClickEvent", function() {
+      var testLabel = "test label";
+      var id = "test_id";
+      var comp;
+      var spyElem = jasmine.createSpyObj("spyElem", [ "bind", "button", "appendTo" ]);
+      spyElem.bind.and.returnValue(spyElem);
+      spyElem.button.and.returnValue(spyElem);
+      spyElem.appendTo.and.returnValue(spyElem);
+      beforeEach(function() {
+        comp = new ScopedPentahoButtonComponent();
+        comp.htmlObject = id;
+        comp.viewReportButtonRegistered = false;
+        comp.label = testLabel;
+        spyOn($.fn, "empty");
+        spyOn($.fn, "text").and.returnValue(spyElem);
+      });
+
+      it("should register event if it's not registered", function() {
+        comp.registerSubmitClickEvent();
+      });
+
+      it("should not register event if already registered", function() {
+        comp.registerSubmitClickEvent();
+        comp.registerSubmitClickEvent();
+      });
+
+      afterEach(function() {
+        expect(comp.viewReportButtonRegistered).toBeTruthy();
+        expect($.fn.empty).toHaveBeenCalled();
+        expect($.fn.text).toHaveBeenCalledWith(testLabel);
+        expect(spyElem.bind.calls.argsFor(0)).toEqual([ "mousedown", jasmine.any(Function) ]);
+        expect(spyElem.bind.calls.argsFor(1)).toEqual([ "click", jasmine.any(Function) ]);
+        expect(spyElem.button).toHaveBeenCalled();
+        expect(spyElem.appendTo).toHaveBeenCalled();
+      });
+    });
+
+    it("should execute expressionStart fn", function() {
+      var comp = new ScopedPentahoButtonComponent();
+      comp.expressionStart();
+    });
+  });
+});
