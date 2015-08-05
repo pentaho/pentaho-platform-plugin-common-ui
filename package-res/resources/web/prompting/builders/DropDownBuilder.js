d2150650d52db04a43e70b373d74e017c7f4a62c
Rob Fellows <rfellows@pentaho.com>
2015-08-05 10:03:53 -0400

    [BACKLOG-4041] - Dev doc for Dropdown Builder

--- package-res/resources/web/prompting/builders/DropDownBuilder.js
+++ package-res/resources/web/prompting/builders/DropDownBuilder.js
@@ -15,10 +15,55 @@
  *
  */
 
+/**
+ * <h2>The Drop Down Builder</h2>
+ *
+ * To use the DropDownBuilder you should require the appropriate file
+ * from Common-Ui:
+ *
+ * <pre><code>
+ *   require(['common-ui/builders/DropDownBuilder'],
+ *     function(DropDownBuilder) {
+ *
+ *     }
+ *   );
+ * </code></pre>
+ *
+ * To get the component you'll have to create a new instance of the builder and
+ * call the <code>build</code> method:
+ *
+ * <pre><code>
+ *   var dropDownBuilder = new DropDownBuilder();
+ *
+ *   var dropDownComponent = dropDownBuilder.build(args);
+ * </code></pre>
+ *
+ * where 'args' is an object that contains the prompt panel and the parameters
+ * necessary for the component.
+ * {@link http://localhost:8080/pentaho/api/repos/:public:plugin-samples:pentaho-cdf:pentaho-cdf-require:30-documentation:30-component_reference:10-core:16-SelectComponent:select_component.xcdf/generatedContent SelectComponent} is created.
+ *
+ * <p>
+ *   Note: the CDF documentation points to the Dashboard located on the Pentaho BI Server
+ * </p>
+ *
+ * @name DropDownBuilder
+ * @class
+ * @extends ValueBasedParameterWidgetBuilder
+ */
 define(['cdf/components/SelectComponent', './ValueBasedParameterWidgetBuilder'],
     function(SelectComponent, ValueBasedParameterWidgetBuilder){
 
   return ValueBasedParameterWidgetBuilder.extend({
+    /**
+     * Method used to build the widget, returning an instance of CDF SelectComponent
+     *
+     * @name DropDownBuilder#build
+     * @method
+     *
+     * @param   {Object}          args - The object with the properties to build the component according to [the CDF Documentation]{@link http://localhost:8080/pentaho/api/repos/:public:plugin-samples:pentaho-cdf:pentaho-cdf-require:30-documentation:30-component_reference:10-core:16-SelectComponent:select_component.xcdf/generatedContent SelectComponent}
+     * @param   {Parameter}       args.param - The parameter with the properties needed to build the component
+     * @returns {SelectComponent} The instance of SelectComponent
+     */
     build: function(args) {
       var widget = this.base(args);
 
