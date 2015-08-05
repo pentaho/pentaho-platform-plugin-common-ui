291de3c4a85d66a7cfbe9b661342516ca665222d
Diogo Mariano <dmariano@pentaho.com>
2015-08-05 13:39:39 +0100

    [BACKLOG-4040] - Review Component - List - Included Dev documentation in the ListBuilder, and its parent classes

--- package-res/resources/web/prompting/builders/ListBuilder.js
+++ package-res/resources/web/prompting/builders/ListBuilder.js
@@ -15,11 +15,58 @@
  *
  */
 
-define(['cdf/components/SelectComponent', 'cdf/components/SelectMultiComponent',
-    './ValueBasedParameterWidgetBuilder'],
+/**
+ * <h2>The List Builder</h2>
+ *
+ * To use the ListBuilder you should require the appropriate file
+ * from Common-Ui:
+ *
+ * <pre><code>
+ *   require(['common-ui/builders/ListBuilder'],
+ *     function(ListBuilder) {
+ *
+ *     }
+ *   );
+ * </code></pre>
+ *
+ * To get the component you'll have to create a new instance of the builder and
+ * call the <code>build</code> method:
+ *
+ * <pre><code>
+ *   var listBuilder = new ListBuilder();
+ *
+ *   var listComponent = listBuilder.build(args);
+ * </code></pre>
+ *
+ * where 'args' is an object that contains the prompt panel and the parameters
+ * necessary for the component.
+ * The property multiSelect defines what is the component created.
+ * If multiSelect equals <code>true</code>, a {@link http://localhost:8080/pentaho/api/repos/:public:plugin-samples:pentaho-cdf:pentaho-cdf-require:30-documentation:30-component_reference:10-core:19-SelectMultiComponent:select_multi_component.xcdf/generatedContent MultiSelectComponent} is created.
+ * If multiSelect equals <code>false</code>, a {@link http://localhost:8080/pentaho/api/repos/:public:plugin-samples:pentaho-cdf:pentaho-cdf-require:30-documentation:30-component_reference:10-core:16-SelectComponent:select_component.xcdf/generatedContent SelectComponent} is created.
+ *
+ * <p>
+ *   Note: the CDF documentation points to the Dashboard located on the Pentaho BI Server
+ * </p>
+ *
+ * @name ListBuilder
+ * @class
+ * @extends ValueBasedParameterWidgetBuilder
+ */
+
+define(['cdf/components/SelectComponent', 'cdf/components/SelectMultiComponent', './ValueBasedParameterWidgetBuilder'],
     function (SelectComponent, SelectMultiComponent, ValueBasedParameterWidgetBuilder) {
 
       return ValueBasedParameterWidgetBuilder.extend({
+        /**
+         * Method used to build the widget, returning an instance of CDF MultiSelect or Select Component
+         *
+         * @name ListBuilder#build
+         * @method
+         *
+         * @param {Object} args - The object with the properties to build the component
+         * @param {Parameter} args.param - The parameter with the properties needed to build the component
+         * @returns {BaseComponent} The instance of MultiSelect or Select Component
+         */
         build: function (args) {
           var widget = this.base(args);
           $.extend(widget, {
--- package-res/resources/web/prompting/builders/ValueBasedParameterWidgetBuilder.js
+++ package-res/resources/web/prompting/builders/ValueBasedParameterWidgetBuilder.js
@@ -15,10 +15,44 @@
  *
  */
 
+/**
+ * <h2>The Value Based Parameter Widget Builder</h2>
+ *
+ * To use the ValueBasedParameterWidgetBuilder you should require the appropriate file
+ * from Common-Ui:
+ *
+ * <pre><code>
+ *   require(['common-ui/builders/ValueBasedParameterWidgetBuilder'],
+ *     function(ValueBasedParameterWidgetBuilder) {
+ *
+ *     }
+ *   );
+ * </code></pre>
+ *
+ * ValueBasedParameterWidgetBuilder is an abstract class, that needs to be extended by whoever
+ * needs to create a CDF component. This abstraction is used to create and assign the array of
+ * value|label pairs to the component
+ *
+ * @name ValueBasedParameterWidgetBuilder
+ * @class
+ * @extends ParameterWidgetBuilderBase
+ */
+
 define(['./ParameterWidgetBuilderBase'],
     function (ParameterWidgetBuilderBase) {
 
       return ParameterWidgetBuilderBase.extend({
+        /**
+         * Creates an array with the value|label pair that defines the
+         * valuesArray property of a CDF component
+         *
+         * @name ValueBasedParameterWidgetBuilder#_getCDFValuesArray
+         * @method
+         *
+         * @param {Parameter} param - The parameter that stores the values
+         * @returns {Array} The array with the value|label pair
+         * @private
+         */
         _getCDFValuesArray: function (param) {
           var valuesArray = [];
           $.each(param.values, function (i, val) {
@@ -27,6 +61,16 @@ define(['./ParameterWidgetBuilderBase'],
           return valuesArray;
         },
 
+        /**
+         * Assigns to the widget to be built the valuesArray properties
+         *
+         * @name ValueBasedParameterWidgetBuilder#build
+         * @method
+         *
+         * @param {Object} args - The object with the properties to build the component
+         * @param {Parameter} args.param - The parameter with the properties needed to build the component
+         * @returns {Object} The object extended with the CDF properties related with valuesArray
+         */
         build: function (args) {
           var widget = this.base(args);
           return $.extend(widget, {
