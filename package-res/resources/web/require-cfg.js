e2c8bad265f39a48e29f6540831a7aa3432474f0
Duarte Cunha Leão <duarte.leao@gmail.com>
2015-10-21 18:51:14 +0100

    [BISERVER-12644][COMP-1665] Warn on use of pen.define and pen.require

--- package-res/resources/web/require-cfg.js
+++ package-res/resources/web/require-cfg.js
@@ -19,5 +19,15 @@
 require.config(requireCfg);
 
 pen = typeof pen != "undefined" ? pen : {};
-pen.require = require;
-pen.define = define;
+pen.require = function() {
+  if(typeof console !== "undefined" && console.warn) {
+    console.warn("'pen.require' is deprecated and will be removed in a future version. Use 'require' instead.");
+  }
+  return require.apply(null, arguments);
+};
+pen.define = function() {
+  if(typeof console !== "undefined" && console.warn) {
+    console.warn("'pen.define' is deprecated and will be removed in a future version. Use 'define' instead.");
+  }
+  return define.apply(null, arguments);
+};
