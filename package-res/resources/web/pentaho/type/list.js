58e9940d14071e9dbaee3df176c6a8b30013eeba
Nelson Antunes <nelson.antunes@webdetails.pt>
2016-01-27 15:17:46 +0000

    [BACKLOG-6051] List validation

--- package-res/resources/web/pentaho/type/list.js
+++ package-res/resources/web/pentaho/type/list.js
@@ -496,6 +496,27 @@ define([
       },
       //endregion
 
+      //region validation
+
+      /**
+       * Performs validation of this item.
+       *
+       * When invalid, returns either one `Error` or a non-empty array of `Error` objects.
+       * When valid, `null` is returned.
+       *
+       * @return {Error|Array.<!Error>|null} An `Error`, a non-empty array of `Error` or `null`.
+       */
+      validate: function() {
+        var errors = [];
+        for(var i = 0, ic = this._elems.length; i !== ic; ++i) {
+          Array.prototype.push.apply(errors, this.meta.of.validate(this._elems[i]));
+        }
+
+        return errors.length > 0 ? errors : null;
+      },
+
+      //endregion
+
       meta: /** @lends pentaho.type.List.Meta# */{
 
         id: module.id,
