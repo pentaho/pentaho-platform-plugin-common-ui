5cb85d9ee14c2c59c634e5f4e914732ec515d150
davidmsantos90 <david.santos@webdetails.pt>
2016-02-15 18:58:45 +0000

    [BACKLOG-5984] Added unit tests for pentaho/lang/Event.js

--- test-js/unit/pentaho/lang/Event.Spec.js
+++ test-js/unit/pentaho/lang/Event.Spec.js
@@ -14,42 +14,87 @@
  * limitations under the License.
  */
 define([
-  ""
+  "pentaho/lang/Event",
+  "pentaho/lang/EventSource"
 ], function() {
   "use strict";
 
-  describe("pentaho.lang.Event -", function() {
+  describe("pentaho.lang.Event -", function(Event, EventSource) {
+    var event, nc_event, source;
+    beforeEach(function() {
+      source = new EventSource();
+
+      event = new Event("foo", source, true);
+      nc_event = new Event("bar", source, false);
+    });
+
+    xit("should be defined.", function() {
+      expect(typeof Event).toBeDefined();
+    });
 
     describe("#type -", function() {
-      xit("should return the proper type when constructing an event of a type", function() {
-        expect(true).toBe(false);
+      xit("should return the proper type when constructing an event of a type.", function() {
+        expect(event.type()).toBe("foo");
       });
     }); // #type
 
     describe("#source -", function() {
-
-      xit("should return the first emitter of the event", function() {
-        expect(true).toBe(false);
+      xit("should return the object where the event was initially emitted.", function() {
+        expect(event.source).toBe(source);
       });
-
     }); // #source
 
-
-    describe("#cancel() -", function() {
-
-      xit("should mark the event as cancelled", function() {
-        expect(true).toBe(false);
+    describe("#isCancelable -", function() {
+      xit("should return `true` if the event is cancelable.", function() {
+        expect(event.isCancelable()).toBe(true);
       });
 
+      xit("should return `false` if the event isn't cancelable.", function() {
+        expect(nc_event.isCancelable()).toBe(false);
+      });
+    }); // #isCancelable
+
+    describe("#cancel() -", function() { //and #isCanceled()
+      var expectCancelEvent = function(e, initial, final) {
+        expect(e.isCanceled()).toBe(initial);
+        e.cancel();
+        expect(e.isCanceled()).toBe(final);
+      };
+
+      xit("should mark the event as canceled if the event is cancelable.", function() {
+        expectCancelEvent(event, false, true);
+      });
+
+      xit("should have no effect if the event is cancelable.", function() {
+        expectCancelEvent(nc_event, false, false);
+      });
+
+      xit("should have no effect if the event is already canceled.", function() {
+        expectCancelEvent(event, false, true);
+        expectCancelEvent(event, true, true);
+      });
     }); // #cancel()
 
-    describe("#isCanceled() -", function() {
+    describe("#clone -", function() {
+      var clone = event.clone();
 
-      xit("should return `true` after calling cancel()", function() {
-        expect(true).toBe(false);
+      xit("the cloned event should not be the same as the original", function() {
+        expect(clone).not.toBe(event);
       });
 
-    }); // #isCanceled()
+      xit("The cloned event should have the same properties values as the original", function() {
+        expect(clone.type).toBe(event.type);
+        expect(clone.source).toBe(event.source);
+        expect(clone.isCancelable()).toBe(event.isCancelable());
+      });
+
+      xit("should not be canceled.", function() {
+        event.cancel();
+        expect(event.isCanceled()).toBe(true);
+        clone = event.clone();
+        expect(clone.isCanceled()).toBe(false);
+      })
+    }); // #clone()
 
   }); // #pentaho.lang.Event
 });
