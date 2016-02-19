7fbac62c438f568e611713c42d3b83238df9497b
Carlos Russo <carlos.russo@webdetails.pt>
2016-02-19 18:25:31 +0000

    [BACKLOG-6419] Initial skeleton of the Filter.

---
+++ test-js/unit/pentaho/lang/Filter.Spec.js
@@ -0,0 +1,54 @@
+/*!
+ * Copyright 2010 - 2016 Pentaho Corporation.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+define([
+  "pentaho/lang/Filter"
+], function(Filter) {
+  "use strict";
+
+  describe("Filter", function() {
+
+    var data, filter;
+    beforeEach(function() {
+      data = {
+        model: [
+          {name: "country", type: "string", label: "Country"},
+          {name: "sales", type: "number", label: "Sales"}
+        ],
+        rows: [
+          {c: [{v: "Portugal"}, {v: 12000}]},
+          {c: [{v: "Ireland"}, {v: 6000}]}
+        ]
+      };
+
+      filter = new Filter.Root();
+    });
+
+    it("", function() {
+      var leaf = new Filter.IsEqual("country", "Portugal");
+      filter.insert(leaf);
+
+      expect(filter.filter(data)).equals({
+        model: [
+          {name: "country", type: "string", label: "Country"},
+          {name: "sales", type: "number", label: "Sales"}
+        ],
+        rows: [
+          {c: [{v: "Portugal"}, {v: 12000}]}
+        ]
+      });
+    });
+  });
+});
