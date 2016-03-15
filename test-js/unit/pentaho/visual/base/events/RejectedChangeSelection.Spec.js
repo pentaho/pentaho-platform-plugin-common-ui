bb24fdf92f24af692394d05bf76045d9cd45c92b
Nelson Antunes <nantunes@pentaho.com>
2016-03-15 14:04:30 +0000

    [BACKLOG-5985][BACKLOG-5989] RejectedSelect, RejectedChangeSelection and RejectedExecute unit testing

---
+++ test-js/unit/pentaho/visual/base/events/RejectedChangeSelection.Spec.js
@@ -0,0 +1,84 @@
+/*!
+ * Copyright 2010 - 2016 Pentaho Corporation.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+define([
+  "pentaho/lang/Event",
+  "pentaho/visual/base/events/RejectedChangeSelection",
+  "tests/pentaho/util/errorMatch"
+], function(Event, RejectedChangeSelection, errorMatch) {
+  "use strict";
+
+  /* global describe:false, it:false, expect:false, beforeEach:false */
+
+  describe("pentaho.events.RejectedChangeSelection -", function() {
+    var type = "change:selectionFilter";
+
+    it("should extend Event", function() {
+      expect(RejectedChangeSelection.prototype instanceof Event).toBe(true);
+    });
+
+    it("static property type should return full type name", function() {
+      expect(RejectedChangeSelection.type).toBe("rejected:" + type);
+    });
+
+    it("static property type should be read-only", function() {
+      expect(function() {
+        RejectedChangeSelection.type = "New Name";
+      }).toThrowError(TypeError);
+    });
+
+    describe("instances -", function() {
+      var event;
+
+      var error = "no go!";
+      var filter = {};
+
+      beforeEach(function() {
+        event = new RejectedChangeSelection({}, error, filter);
+      });
+
+      it("should extend Event", function() {
+        expect(event instanceof Event).toBe(true);
+      });
+
+      it("error property should be the same than received in the constructor", function() {
+        expect(event.error).toBe(error);
+      });
+
+      it("dataFilter property should be the same than received in the constructor", function() {
+        expect(event.dataFilter).toBe(filter);
+      });
+
+      it("dataFilter property should be immutable", function() {
+        expect(function() {
+          event.dataFilter = "other";
+        }).toThrowError(TypeError);
+      });
+    });
+
+    it("should throw if empty error parameter", function() {
+      expect(function() {
+        return new RejectedChangeSelection({});
+      }).toThrow(errorMatch.argRequired("error"));
+    });
+
+    it("should throw if empty dataFilter parameter", function() {
+      expect(function() {
+        return new RejectedChangeSelection({}, "no go!");
+      }).toThrow(errorMatch.argRequired("dataFilter"));
+    });
+
+  }); // #pentaho.events.RejectedChangeSelection
+});
---
+++ test-js/unit/pentaho/visual/base/events/RejectedExecute.Spec.js
@@ -0,0 +1,84 @@
+/*!
+ * Copyright 2010 - 2016 Pentaho Corporation.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+define([
+  "pentaho/lang/Event",
+  "pentaho/visual/base/events/RejectedExecute",
+  "tests/pentaho/util/errorMatch"
+], function(Event, RejectedExecute, errorMatch) {
+  "use strict";
+
+  /* global describe:false, it:false, expect:false, beforeEach:false */
+
+  describe("pentaho.events.RejectedExecute -", function() {
+    var type = "execute";
+
+    it("should extend Event", function() {
+      expect(RejectedExecute.prototype instanceof Event).toBe(true);
+    });
+
+    it("static property type should return full type name", function() {
+      expect(RejectedExecute.type).toBe("rejected:" + type);
+    });
+
+    it("static property type should be read-only", function() {
+      expect(function() {
+        RejectedExecute.type = "New Name";
+      }).toThrowError(TypeError);
+    });
+
+    describe("instances -", function() {
+      var event;
+
+      var error = "no go!";
+      var filter = {};
+
+      beforeEach(function() {
+        event = new RejectedExecute({}, error, filter);
+      });
+
+      it("should extend Event", function() {
+        expect(event instanceof Event).toBe(true);
+      });
+
+      it("error property should be the same than received in the constructor", function() {
+        expect(event.error).toBe(error);
+      });
+
+      it("dataFilter property should be the same than received in the constructor", function() {
+        expect(event.dataFilter).toBe(filter);
+      });
+
+      it("dataFilter property should be immutable", function() {
+        expect(function() {
+          event.dataFilter = "other";
+        }).toThrowError(TypeError);
+      });
+    });
+
+    it("should throw if empty error parameter", function() {
+      expect(function() {
+        return new RejectedExecute({});
+      }).toThrow(errorMatch.argRequired("error"));
+    });
+
+    it("should throw if empty dataFilter parameter", function() {
+      expect(function() {
+        return new RejectedExecute({}, "no go!");
+      }).toThrow(errorMatch.argRequired("dataFilter"));
+    });
+
+  }); // #pentaho.events.RejectedExecute
+});
