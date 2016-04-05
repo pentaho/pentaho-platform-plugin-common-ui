277ff5af5d49c2f243d84669e847b142a321c69b
Nelson Antunes <nantunes@pentaho.com>
2016-04-05 18:08:36 +0100

    [BACKLOG-6733] pentaho/lang/SortedList

---
+++ package-res/resources/web/pentaho/lang/SortedList.js
@@ -0,0 +1,228 @@
+/*!
+ * Copyright 2010 - 2016 Pentaho Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+define([
+  "./List",
+  "./Base"
+], function(List, Base) {
+
+  var baseProto = Base.Array.prototype;
+
+  return List.extend("pentaho.lang.SortedList", /** @lends  pentaho.lang.SortedList# */{
+    /**
+     * @classdesc The `SortedList` class is an abstract base class for ordered arrays.
+     *
+     * ### AMD
+     *
+     * To obtain the constructor of this class,
+     * require the module `"pentaho/lang/SortedList"`.
+     *
+     * ### Remarks
+     *
+     * If an attempt is made to add a element to a specific index of the list
+     * an error is thrown.
+     *
+     * @class
+     * @name SortedList
+     * @memberOf pentaho.lang
+     * @abstract
+     * @extends pentaho.lang.List
+     *
+     * @description Initializes a sorted list instance.
+     *
+     * Note that because a `SortedList` is a sub-class of `Array`,
+     * it cannot be a "newable" constructor function (at least up to ECMAScript version 5).
+     * Instead, instances of `SortedList` are actually initial instances of `Array`
+     * whose prototype is then changed to be that of `SortedList`.
+     *
+     * In other words, `SortedList` is an "initialization" constructor
+     * (see {@link pentaho.lang.ISpecifiable} for more information
+     *  on these concepts).
+     *
+     * Concrete `SortedList` sub-classes should provide a static `to` method
+     * to help in their construction.
+     *
+     * @param {Object}   [keyArgs]         The keyword arguments.
+     * @param {Function} [compareFunction] Specifies a function that defines the sort order.
+     *
+     * These are not used directly by the `SortedList` class
+     * but are passed-through to the methods that handle
+     * the initialization of each list element.
+     */
+    constructor: function(keyArgs, compareFunction) {
+      if (compareFunction) {
+        this.compare = compareFunction;
+      }
+
+      this.base(keyArgs);
+    },
+
+    _compareFunction: function(e1, e2) {
+      if (e1 == null || e2 == null) {
+        if (e1 != null) {
+          return 1;
+        }
+
+        if (e2 != null) {
+          return -1;
+        }
+
+        return 0;
+      }
+
+      var v1 = e1.valueOf();
+      var v2 = e2.valueOf();
+
+      return v1 === v2 ? 0 : (v1 > v2 ? 1 : -1);
+    },
+
+    get compare() {
+      return this._compareFunction;
+    },
+
+    set compare(compareFunction) {
+      this._compareFunction = compareFunction;
+    },
+
+    sort: function(compareFunction) {
+      baseProto.sort.call(this, compareFunction || this._compareFunction);
+    },
+
+    /**
+     * Performs a binary search on the sorted list.
+     *
+     * @param {*} elem The item to search for.
+     *
+     * @return {Number} The index of the element;
+     *                  when not found returns the negative
+     *                  index of the closest element.
+     */
+    search: function(elem) {
+      var left = 0;
+      var right = this.length - 1;
+
+      while (left <= right) {
+        var i = (left + right) / 2 | 0;
+
+        var comparison = this.compare(this[i], elem);
+
+        if (comparison < 0) {
+          left = i + 1;
+        } else if (comparison > 0) {
+          right = i - 1;
+        } else {
+          return i;
+        }
+      }
+
+      return ~right;
+    },
+
+    copyWithin: function() {
+      throw new Error("Can't copy within a sorted list.");
+    },
+
+    fill: function() {
+      throw new Error("Can't fill a sorted list.");
+    },
+
+    reverse: function() {
+      throw new Error("Can't reverse a sorted list.");
+    },
+
+    unshift: function() {
+      throw new Error("Can't do a indexed insert in a sorted list.");
+    },
+
+    insert: function() {
+      throw new Error("Can't do a indexed insert in a sorted list.");
+    },
+
+    replace: function() {
+      throw new Error("Can't do a indexed replace in a sorted list.");
+    },
+
+    splice: function() {
+      if (arguments.length > 2) {
+        throw new Error("Can't do a indexed insert in a sorted list.");
+      }
+
+      baseProto.splice.apply(this, arguments);
+    },
+
+    _insertInOrder: function(elem, keyArgs) {
+      var elem2 = this._adding(elem, null, keyArgs);
+
+      if (elem2 !== undefined) {
+        var index = Math.abs(this.search(elem2));
+        baseProto.splice.call(this, index, 0, elem2);
+
+        if (this._added) {
+          this._added(elem2, index, keyArgs);
+        }
+      }
+
+      return elem2;
+    },
+
+    _addMany: function(elems, keyArgs) {
+      var isReplay = elems === this;
+      var LE = elems.length;
+      if (!LE) {
+        return this.length;
+      }
+
+      var i = 0;
+
+      if (isReplay) {
+        while (i < LE) {
+          var elem = elems[i];
+
+          var elem2 = this._adding(elem, i, keyArgs);
+
+          if (elem2 === undefined) {
+            // Not added afterall
+            // Remove from the array
+            baseProto.splice.call(this, i, 1);
+
+            LE--;
+            continue;
+          } else {
+            if (elem !== elem2) {
+              this[i] = elem2;
+            }
+
+            if (this._added) {
+              this._added.call(this, elem2, i, keyArgs);
+            }
+          }
+
+          i++;
+        }
+
+        // sort initial array
+        this.sort();
+      } else {
+        while (i < LE) {
+          this._insertInOrder(elems[i], keyArgs);
+
+          i++;
+        }
+      }
+
+      return LE;
+    }
+  });
+});
---
+++ test-js/unit/pentaho/lang/SortedList.Spec.js
@@ -0,0 +1,432 @@
+/*!
+ * Copyright 2010 - 2016 Pentaho Corporation.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+define([
+  "pentaho/lang/SortedList",
+  "pentaho/lang/List",
+  "pentaho/lang/Base"
+], function(SortedList, List, Base) {
+  "use strict";
+
+  /* global describe:false, it:false, expect:false, beforeEach:false */
+
+  describe("pentaho.lang.SortedList -", function() {
+    it("should be defined", function() {
+      expect(SortedList).toBeDefined();
+    });
+
+    describe("direct use", function() {
+      var reverseSort = function(e1, e2) {
+        var v1 = e1.valueOf();
+        var v2 = e2.valueOf();
+
+        return v1 === v2 ? 0 : (v1 > v2 ? -1 : 1);
+      };
+
+      var unsortedValues;
+
+      var expectedDefaultOrder;
+      var expectedCustomOrder;
+
+      beforeEach(function() {
+        unsortedValues = [5, 2, 3, 6, 1, 4];
+        expectedDefaultOrder = [1, 2, 3, 4, 5, 6];
+        expectedCustomOrder = [6, 5, 4, 3, 2, 1];
+      });
+
+      describe("construction", function() {
+        describe("constructor", function() {
+          var list;
+
+          beforeEach(function() {
+            list = new SortedList();
+          });
+
+          it("should be defined", function() {
+            expect(list).toBeDefined();
+          });
+
+          it("should be a SortedList", function() {
+            expect(list instanceof SortedList).toBe(true);
+          });
+
+          it("should be a List", function() {
+            expect(list instanceof List).toBe(true);
+          });
+
+          it("should be a Array", function() {
+            expect(list instanceof Array).toBe(true);
+          });
+        });
+
+        describe("cast with .to(...)", function() {
+          var list;
+
+          beforeEach(function() {
+            list = SortedList.to([1, 2]);
+          });
+
+          it("should be a SortedList", function() {
+            expect(list instanceof SortedList).toBe(true);
+          });
+
+          it("should be a List", function() {
+            expect(list instanceof List).toBe(true);
+          });
+
+          it("should be a Array", function() {
+            expect(list instanceof Array).toBe(true);
+          });
+        });
+      });
+
+      describe("default sort", function() {
+        describe("sort on cast with .to(...)", function() {
+          var list;
+
+          beforeEach(function() {
+            list = SortedList.to(unsortedValues);
+          });
+
+          it("should be sorted", function() {
+            expect(list.slice()).toEqual(expectedDefaultOrder);
+          });
+        });
+
+        describe("sort on push", function() {
+          var list;
+
+          beforeEach(function() {
+            list = new SortedList();
+          });
+
+          it("should sort on push", function() {
+            unsortedValues.forEach(function(e) {
+              list.push(e);
+            });
+
+            expect(list.slice()).toEqual(expectedDefaultOrder);
+          });
+
+          it("should sort on multi-value push", function() {
+            list.push(5, 2, 3, 6, 1, 4);
+
+            expect(list.slice()).toEqual([1, 2, 3, 4, 5, 6]);
+          });
+
+          it("should accept null values", function() {
+            list.push(3, 2, null, 1);
+
+            expect(list.slice()).toEqual([null, 1, 2, 3]);
+          });
+
+          it("should ignore undefined values", function() {
+            list.push(3, 2, undefined, 1);
+
+            expect(list.slice()).toEqual([1, 2, 3]);
+          });
+
+          it("should accept repeated values", function() {
+            list.push(3, 3, 3, 3, 5, 1);
+
+            expect(list.slice()).toEqual([1, 3, 3, 3, 3, 5]);
+          });
+        });
+      });
+
+      describe("custom sort", function() {
+        describe("sort on cast with .to(...)", function() {
+          var list;
+
+          beforeEach(function() {
+            list = SortedList.to(unsortedValues, null, reverseSort);
+          });
+
+          it("should be sorted", function() {
+            expect(list.slice()).toEqual(expectedCustomOrder);
+          });
+        });
+
+        describe("sort on push", function() {
+          var list;
+
+          beforeEach(function() {
+            list = new SortedList(null, reverseSort);
+          });
+
+          it("should sort on push", function() {
+            unsortedValues.forEach(function(e) {
+              list.push(e);
+            });
+
+            expect(list.slice()).toEqual(expectedCustomOrder);
+          });
+        });
+      });
+    });
+
+    describe("extended use", function() {
+      var elementSort = function(e1, e2) {
+        if (e1 == null || e2 == null) {
+          if (e1 != null) {
+            return 1;
+          }
+
+          if (e2 != null) {
+            return -1;
+          }
+
+          return 0;
+        }
+
+        // order by increment and only then by index
+        var v1 = e1.increment;
+        var v2 = e2.increment;
+
+        if (v1 === v2) {
+          v1 = e1.index;
+          v2 = e2.index;
+        }
+
+        return v1 === v2 ? 0 : (v1 > v2 ? 1 : -1);
+      };
+
+      var Element = Base.extend({
+        index: 0,
+        increment: 0,
+
+        constructor: function(index, increment) {
+          this.index = index;
+          this.increment = increment || 0;
+        },
+
+        toString: function() {
+          return this.index + " (" + this.increment + ")";
+        }
+      }, {
+        to: function(value) {
+          if (value instanceof Element) {
+            return value;
+          }
+
+          if (typeof value === 'number') {
+            return new Element(value);
+          }
+
+          if (value === null) {
+            return null;
+          }
+        }
+      });
+
+      var e1 = new Element(1, 2);
+      var e2 = new Element(2, 2);
+      var e3 = new Element(3, 1);
+      var e4 = new Element(4, 3);
+      var e5 = new Element(5, 2);
+      var e6 = new Element(6, 3);
+
+      var unsortedValues;
+
+      var expectedDefaultOrder;
+      var expectedCustomOrder;
+
+      var SortedElements = SortedList.extend({
+        elemClass: Element
+      });
+
+      beforeEach(function() {
+        unsortedValues = [e5, e2, e3, e6, e1, e4];
+        expectedDefaultOrder = [e1, e2, e3, e4, e5, e6];
+        expectedCustomOrder = [e3, e1, e2, e5, e4, e6];
+      });
+
+      describe("construction", function() {
+        describe("constructor", function() {
+          var list;
+
+          beforeEach(function() {
+            list = new SortedElements();
+          });
+
+          it("should be defined", function() {
+            expect(list).toBeDefined();
+          });
+
+          it("should be a SortedElements", function() {
+            expect(list instanceof SortedElements).toBe(true);
+          });
+
+          it("should be a SortedList", function() {
+            expect(list instanceof SortedList).toBe(true);
+          });
+
+          it("should be a List", function() {
+            expect(list instanceof List).toBe(true);
+          });
+
+          it("should be a Array", function() {
+            expect(list instanceof Array).toBe(true);
+          });
+        });
+
+        describe("cast with .to(...)", function() {
+          var list;
+
+          beforeEach(function() {
+            list = SortedElements.to([1, 2]);
+          });
+
+          it("should be a SortedElements", function() {
+            expect(list instanceof SortedElements).toBe(true);
+          });
+
+          it("should be a SortedList", function() {
+            expect(list instanceof SortedList).toBe(true);
+          });
+
+          it("should be a List", function() {
+            expect(list instanceof List).toBe(true);
+          });
+
+          it("should be a Array", function() {
+            expect(list instanceof Array).toBe(true);
+          });
+
+          it("should cast elements", function() {
+            expect(list[0] instanceof Element).toBe(true);
+            expect(list[1] instanceof Element).toBe(true);
+          });
+        });
+      });
+
+      describe("default sort", function() {
+        describe("sort on cast with .to(...)", function() {
+          var list;
+
+          beforeEach(function() {
+            list = SortedElements.to(unsortedValues);
+          });
+
+          it("should be sorted", function() {
+            expect(list.slice()).toEqual(expectedDefaultOrder);
+          });
+        });
+
+        describe("sort on push", function() {
+          var list;
+
+          beforeEach(function() {
+            list = new SortedElements();
+          });
+
+          it("should sort on push", function() {
+            unsortedValues.forEach(function(e) {
+              list.push(e);
+            });
+
+            expect(list.slice()).toEqual(expectedDefaultOrder);
+          });
+        });
+      });
+
+      describe("custom sort", function() {
+        describe("sort on cast with .to(...)", function() {
+          var list;
+
+          beforeEach(function() {
+            list = SortedElements.to(unsortedValues, null, elementSort);
+          });
+
+          it("should be sorted", function() {
+            expect(list.slice()).toEqual(expectedCustomOrder);
+          });
+        });
+
+        describe("sort on push", function() {
+          var list;
+
+          beforeEach(function() {
+            list = new SortedElements(null, elementSort);
+          });
+
+          it("should sort on push", function() {
+            unsortedValues.forEach(function(e) {
+              list.push(e);
+            });
+
+            expect(list.slice()).toEqual(expectedCustomOrder);
+          });
+        });
+      });
+    });
+
+    describe("restrictions", function() {
+      var list;
+
+      beforeEach(function() {
+        list = new SortedList();
+      });
+
+      it("should throw if calling insert", function() {
+        expect(function() {
+          list.insert(5, 0);
+        }).toThrowError();
+      });
+
+      it("should throw if calling replace", function() {
+        expect(function() {
+          list.replace(5, 0, 6);
+        }).toThrowError();
+      });
+
+      it("should throw if adding elements with splice", function() {
+        expect(function() {
+          list.splice(1, 0, 6, 7, 8);
+        }).toThrowError();
+      });
+
+      it("should not throw if calling splice without adding elements", function() {
+        expect(function() {
+          list.splice(0, 0);
+        }).not.toThrowError();
+      });
+
+      it("should throw if calling copyWithin", function() {
+        expect(function() {
+          list.copyWithin(0, 1, 6);
+        }).toThrowError();
+      });
+
+      it("should throw if calling fill", function() {
+        expect(function() {
+          list.fill(5);
+        }).toThrowError();
+      });
+
+      it("should throw if calling reverse", function() {
+        expect(function() {
+          list.reverse();
+        }).toThrowError();
+      });
+
+      it("should throw if calling unshift", function() {
+        expect(function() {
+          list.unshift(5);
+        }).toThrowError();
+      });
+    });
+  });
+});
