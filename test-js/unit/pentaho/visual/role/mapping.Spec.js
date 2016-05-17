a78f85c595f3103a01dd8bf88c1fa7e879ba4abc
Guilherme Raimundo <graimundo@pentaho.com>
2016-05-17 18:00:53 +0100

    [BACKLOG-6797] - Added Unit tests

--- test-js/unit/pentaho/visual/role/mapping.Spec.js
+++ test-js/unit/pentaho/visual/role/mapping.Spec.js
@@ -3,21 +3,35 @@ define([
   "pentaho/type/Context",
   "pentaho/type/complex",
   "pentaho/visual/base",
+  "pentaho/type/value",
   "pentaho/visual/role/mapping",
-  "pentaho/visual/role/mappingAttribute",
-], function(errorMatch, Context, complexFactory, visualModelFactory, mappingFactory, attributeFactory) {
+  "pentaho/visual/role/mappingAttribute"
+], function(errorMatch, Context, complexFactory, visualModelFactory, valueFactory, mappingFactory, attributeFactory) {
   "use strict";
 
   describe("pentaho.visual.role.Mapping", function() {
-    var Complex, Mapping, Attribute, VisualModel;
+    var Complex, Value, Mapping, Attribute, VisualModel;
 
     beforeEach(function () {
       var context = new Context();
       Complex = context.get(complexFactory);
+      Value = context.get(valueFactory);
       Mapping = context.get(mappingFactory);
       Attribute = context.get(attributeFactory);
       VisualModel = context.get(visualModelFactory);
     });
+
+    /**
+     * Creates a visual model that has a property visualRole with the given mapping of type Mapping
+     */
+    function createVisualModelAndMapping(Mapping) {
+      var DerivedVisualModel = VisualModel.extend({type: {
+        props: [{name: "visualRole", type: Mapping }]
+      }});
+      var visualModel = new DerivedVisualModel();
+
+      return visualModel;
+    }
 
     function assertIsValid(complex) {
       // this way, errors are shown in the console...
@@ -28,41 +42,61 @@ define([
       expect(complex.isValid).toBe(false);
     }
 
+    function assertEqualSet( arrayA, arrayB ) {
+      expect(arrayA.length).toBe(arrayB.length);
+      for(var i = 0; i < arrayA.length; i++ ) {
+        expect(arrayA[i]).toEqual(arrayB[i]);
+      }
+    }
+
     function getValue( object ) {
       return object.value;
     }
 
     describe("#levelEffective", function () {
-      it("when the level is null, the effective level should be equal to the auto level", function () {
-        /* When {@link pentaho.visual.role.Mapping#level} is not `null`,
-        * that measurement level is returned.
-        * Otherwise,
-        * the value of {@link pentaho.visual.role.Mapping#levelAuto},
-        * which can be `undefined`, is returned.
-        */
-        fail();
+      it("should be equal to the auto level when the level is null", function () {
+        var BaseMapping = Mapping.extend({ type: { levels: ["nominal", "ordinal"]}});
+
+        // make sure that the mapping has a model
+        var model = createVisualModelAndMapping(BaseMapping);
+        var mapping = model.visualRole;
+
+        // Assumptions
+        expect(mapping.level).toBe(null);
+
+        // Test
+        var autoLevel = mapping.levelAuto;
+        var levelEffective = mapping.levelEffective;
+        expect(levelEffective).toEqual(autoLevel);
+
       });
 
-      it("when the level is not null, the effective level should be equal to the level", function () {
-        fail();
+      it("should be equal to the level, when the level is not null", function () {
+        var BaseMapping = Mapping.extend({ type: { levels: ["nominal", "ordinal"]}});
+        var model = createVisualModelAndMapping(BaseMapping);
+        var mapping = model.visualRole;
+
+        var expectedEffectiveLevel = "nominal";
+        mapping.level = expectedEffectiveLevel;
+
+        // Test
+        var actualLevelEffective = mapping.levelEffective;
+        expect(actualLevelEffective).toEqual(expectedEffectiveLevel);
+
       });
 
     });
 
     describe("#levelAuto", function () {
-      it("when a valid mapping is empty, the auto level is undefined", function() {
+      it("is undefined, when the mapping is valid and empty", function() {
         var ValidMapping = Mapping.extend( {
           type: {
             levels: ["nominal"]
           }
         });
 
-        var DerivedVisualModel = VisualModel.extend({type: {
-          props: [{name: "x", type: ValidMapping}]
-        }});
-
-        var visualModel = new DerivedVisualModel();
-        var mapping = visualModel.x;
+        var model = createVisualModelAndMapping(ValidMapping);
+        var mapping = model.visualRole;
 
         // Assumptions
         assertIsValid(mapping);
@@ -72,7 +106,7 @@ define([
         expect(mapping.levelAuto).toBeUndefined();
       });
 
-      it("when a non-empty mapping is invalid, the auto level is undefined", function() {
+      it("is undefined, when the mapping is not empty and is invalid", function() {
         var InvalidMapping = Mapping.extend( {
           type: {
             levels: ["nominal"],
@@ -121,7 +155,7 @@ define([
     describe(".Type", function () {
 
       describe("#levels", function () {
-        it("set levels are set", function() {
+        it("are set when set", function() {
           var MyMapping = Mapping.extend();
 
           var expectedLevels = ["nominal", "ordinal"];
@@ -132,7 +166,7 @@ define([
           expect(actualLevels).toEqual(expectedLevels);
         });
 
-        it("a non-abstract visual role needs to support at least one measurement level", function () {
+        it("should throw when a non-abstract mapping does not support any measurement level", function () {
           function defineEmptyLevelsMapping() {
             Mapping.extend( { type: { levels: [] } });
           }
@@ -177,22 +211,30 @@ define([
           expect(extendMapping).toThrow(errorMatch.argInvalid("levels"));
         });
 
-        it("set levels is additive", function() {
-          fail();
+        it("setting levels always adds to the levels of the parent mapping", function() {
+          var baseLevels = ["nominal"];
+          var BaseMapping = Mapping.extend({type: {levels: baseLevels}});
+
+          var addLevels = ["ordinal"];
+          var DerivedMapping = BaseMapping.extend({type: {levels: addLevels}});
+
+          var expectedLevels = baseLevels.concat(addLevels);
+          var actualLevels = DerivedMapping.type.levels.toArray(getValue);
+          assertEqualSet(expectedLevels, actualLevels);
         });
 
         it("a visual role definition cannot remove support for a measurement level supported by its ancestors", function () {
-          fail("Test no longer makes sense. Check...");
-          // TODO: This test stopped making sens. Levels are additive
           var baseLevels = ["nominal", "ordinal"];
           var BaseMapping = Mapping.extend( { type: { levels: baseLevels } });
 
           var constrainedLevels = [baseLevels[0]];
-          function extendMapping() {
-            BaseMapping.extend( { type: { levels: constrainedLevels } } );
-          }
+          var DerivedMapping = BaseMapping.extend( { type: { levels: constrainedLevels } } );
 
-          expect(extendMapping).toThrow(errorMatch.argInvalid("levels"));
+          var actualLevels = DerivedMapping.type.levels.toArray(getValue);
+          expect(actualLevels.length).toBe(2);
+          expect(actualLevels).toContain("nominal");
+          expect(actualLevels).toContain("ordinal");
+
         });
 
         it("when a visual role definition does not specify the measurement levels, it inherits the ones of its parent visual role definition", function () {
@@ -252,7 +294,12 @@ define([
           var dataTypeA = Complex.extend().type;
           var dataTypeB = Complex.extend().type;
 
-          var BaseMapping = Mapping.extend({type: {dataType: dataTypeA}});
+          var BaseMapping = Mapping.extend({
+            type: {
+              levels: ["nominal"],
+              dataType: dataTypeA
+            }
+          });
 
           function deriveToNonSubDataType() {
             BaseMapping.extend( {type: {dataType: dataTypeB}});
@@ -262,28 +309,53 @@ define([
         });
 
         it("a dataType can be a subtype of the dataType of the parent visual role definition", function () {
-          fail();
+          var Parent = Complex.extend();
+          var parentDataType = Parent.type;
+          var childDataType = Parent.extend().type;
+
+          var BaseMapping = Mapping.extend({
+            type: {
+              levels: ["nominal"],
+              dataType: parentDataType
+            }
+          });
+          var DerivedMapping = BaseMapping.extend( { type: { dataType: childDataType }});
+
+          expect(DerivedMapping.type.dataType).toBe(childDataType);
         });
 
 
         it("when a visual role definition does not specify the supported dataType, it inherits the one defined in its parent visual role definition", function () {
-          fail();
+          var expectedDataType = Complex.extend().type;
+          var BaseMapping = Mapping.extend({
+            type: {
+              levels: ["nominal"],
+              dataType: expectedDataType
+            }
+          });
+          var DerivedMapping = BaseMapping.extend();
+
+          expect(DerivedMapping.type.dataType).toBe(expectedDataType);
         });
 
         it("???", function () {
           /* The first set local value must respect the _monotonicity_ property with the inherited value.*/
-
+          fail();
         });
 
         it("when dataType is set to a Nully value, the operation is ignored", function () {
           fail();
         });
 
-        it("when dataType is set and the visual role definition already has descendants, an error is thrown.", function () {
-          /* @throws {pentaho.lang.OperationInvalidError} When setting and the type already has
-           * [subtypes]{@link pentaho.type.Type#hasDescendants}.
-           */
-          fail();
+        it("should throw when dataType is set and the visual role definition already has descendants", function () {
+          var BaseMapping = Mapping.extend({ type: { levels: ["nominal", "ordinal"] }});
+          var DerivedMapping = BaseMapping.extend();
+
+          function setDataType() {
+            BaseMapping.type.dataType = "string";
+          }
+
+          expect(setDataType).toThrow(errorMatch.operInvalid());
         });
 
         it("???", function() {
@@ -294,12 +366,8 @@ define([
           fail();
         });
 
-        it("the root visual role definition has a dataType of pentaho.type.Value", function () {
-          fail();
-        });
-
-        it("when the dataType removes support of a measurement level supported by the visual role definition ancestors, then an error is thrown", function () {
-          fail();
+        it("is pentaho.type.Value for the root visual role definition", function () {
+          expect(Mapping.type.dataType).toBe(Value.type);
         });
 
         it("should throw when setting a data type that is inherently qualitative and " +
@@ -317,14 +385,6 @@ define([
 
       });
 
-      describe("levelsEffective", function() {
-
-        // TODO: levels effective might be dropped
-        it("a dataType constraints the effective levels", function() {
-          fail();
-        });
-
-      });
     });
   });
 });
