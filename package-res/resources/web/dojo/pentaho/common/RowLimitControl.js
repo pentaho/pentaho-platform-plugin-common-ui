6c6ec285f578131d461a41f6ae40a2c2989ac354
Aliaksandr Kastenka <Aliaksandr_Kastenka@epam.com>
2016-10-14 12:57:53 +0300

    [BACKLOG-11300] - Browser freezes when putting data into disabled row limit textbox in

--- package-res/resources/web/dojo/pentaho/common/RowLimitControl.js
+++ package-res/resources/web/dojo/pentaho/common/RowLimitControl.js
@@ -241,6 +241,11 @@ define(["dojo/_base/declare", "dijit/form/Select", "dijit/form/TextBox", "dijit/
             _setRowsNumberInputDisabled: function (isDisabled) {
               this.rowsNumberInput.set('disabled', isDisabled);
               this.rowsNumberInput.set('readonly', isDisabled);
+              if(isDisabled) {
+                $(".rl_rowsNumberInput").hide();
+              } else {
+                $(".rl_rowsNumberInput").show();
+              }
             },
 
             _setRowLimitRestrictionDisabled: function (isDisabled) {
