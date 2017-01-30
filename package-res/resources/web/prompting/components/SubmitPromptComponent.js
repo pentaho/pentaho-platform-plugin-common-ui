8d90274f063c2c56b7ff9d29f692d28062303322
Egor Zhuk <Yahor_Zhuk@epam.com>
2017-01-30 08:58:34 +0300

    [SP-3212][BACKLOG-13280]-Ensure only one click is needed to submit a report

--- package-res/resources/web/prompting/components/SubmitPromptComponent.js
+++ package-res/resources/web/prompting/components/SubmitPromptComponent.js
@@ -95,19 +95,24 @@ define(['./ScopedPentahoButtonComponent', 'common-ui/jquery-clean'], function(Sc
 
       //BISERVER-13280
       //A blur event on text input prevents execution of the click event if a blur and a click is a single action on UI.
-      //It can be fixed with a timeout, but we also must prevent a double execution by clearing a timeout if the click event still occured.
+      //It can be fixed with a timeout, but we also must prevent a double execution by clearing a timeout if the click event still occurred.
       var button = $('#' + this.htmlObject + ' button');
+      //Unbind existent handlers
+      button.unbind('click');
+      button.unbind('mousedown');
+      //Set timeout on mousedown
       button.mousedown(function(){
+        this.expressionStart();
         this.submitTimeout = setTimeout( function(){
-          this.expression(true);
+          this.expression(false);
           delete this.submitTimeout;
         }.bind(this), 500);
       }.bind(this));
 
       button.click(function(){
-        if(this.submitTimeout){
-          clearTimeout(this.submitTimeout);
-          delete this.submitTimeout;
+        //If there is no timeout we need to call expression
+        if(!this.submitTimeout){
+          this.expression(false);
         }
       }.bind(this));
     },
