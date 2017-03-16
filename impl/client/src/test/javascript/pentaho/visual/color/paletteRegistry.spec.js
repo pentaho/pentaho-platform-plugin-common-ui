33d7bf6a2b6d196abaaf962466fa00fc145c2eb9
Duarte Cunha Leão <duarte.leao@gmail.com>
2017-03-16 13:24:37 +0000

    [BACKLOG-14077] Fix broken paletteRegistry test due to uphill CDF changes.

--- impl/client/src/test/javascript/pentaho/visual/color/paletteRegistry.spec.js
+++ impl/client/src/test/javascript/pentaho/visual/color/paletteRegistry.spec.js
@@ -15,245 +15,252 @@
  */
 define(["pentaho/visual/color/paletteRegistry"], function(singletonPaletteRegistry) {
 
-	var PaletteRegistry = singletonPaletteRegistry && singletonPaletteRegistry.constructor;
+  "use strict";
 
-	describe("PaletteRegistry -", function() {
-		it("should be a function", function() {
-			expect(typeof PaletteRegistry).toBe("function");
-		});
+  /* global describe:false, it:false, expect:false, beforeEach:false, afterEach:false, Promise:false, spyOn:false*/
 
-		describe("#new() -", function() {
-			it("should create an instance of PaletteRegistry", function() {
-				var registry = new PaletteRegistry();
-				expect(registry instanceof PaletteRegistry).toBe(true);
-			});
+  var PaletteRegistry = singletonPaletteRegistry && singletonPaletteRegistry.constructor;
 
-			it("should contain 0 color palettes", function() {
-				var registry = new PaletteRegistry();
-				var palettes = registry.getAll();
-				expect(palettes instanceof Array).toBe(true);
-				expect(palettes.length).toBe(0);
-			});
-		});
+  describe("PaletteRegistry -", function() {
+    it("should be a function", function() {
+      expect(typeof PaletteRegistry).toBe("function");
+    });
 
-		describe("#add({name, colors}) -", function() {
-			it("should throw an error if given no arguments or a nully argument", function() {
-				var registry = new PaletteRegistry();
-				expect(function() {
-					registry.add();
-				}).toThrow();
+    describe("#new() -", function() {
+      it("should create an instance of PaletteRegistry", function() {
+        var registry = new PaletteRegistry();
+        expect(registry instanceof PaletteRegistry).toBe(true);
+      });
 
-				expect(function() {
-					registry.add(null);
-				}).toThrow();
+      it("should contain 0 color palettes", function() {
+        var registry = new PaletteRegistry();
+        var palettes = registry.getAll();
+        expect(palettes instanceof Array).toBe(true);
+        expect(palettes.length).toBe(0);
+      });
+    });
 
-				expect(function() {
-					registry.add(undefined);
-				}).toThrow();
-			});
+    describe("#add({name, colors}) -", function() {
+      it("should throw an error if given no arguments or a nully argument", function() {
+        var registry = new PaletteRegistry();
+        expect(function() {
+          registry.add();
+        }).toThrow();
 
-			it("should add a palette with name that has not been defined yet", function() {
-				var registry = new PaletteRegistry();
-				var palette = {name: "A"};
-				registry.add(palette);
+        expect(function() {
+          registry.add(null);
+        }).toThrow();
 
-				var palettes = registry.getAll();
-				expect(palettes instanceof Array).toBe(true);
-				expect(palettes.length).toBe(1);
+        expect(function() {
+          registry.add(undefined);
+        }).toThrow();
+      });
 
-				expect(palettes[0]).toBe(palette);
-			});
+      it("should add a palette with name that has not been defined yet", function() {
+        var registry = new PaletteRegistry();
+        var palette = {name: "A"};
+        registry.add(palette);
 
-			it("should replace a palette with name that has already been defined", function() {
-				var registry = new PaletteRegistry();
-				var palette1 = {name: "A"};
-				var palette2 = {name: "A"};
-				registry.add(palette1);
-				registry.add(palette2);
+        var palettes = registry.getAll();
+        expect(palettes instanceof Array).toBe(true);
+        expect(palettes.length).toBe(1);
 
-				var palettes = registry.getAll();
-				expect(palettes instanceof Array).toBe(true);
-				expect(palettes.length).toBe(1);
-				expect(palettes[0]).toBe(palette2);
-			});
+        expect(palettes[0]).toBe(palette);
+      });
 
-			it("should add and retain multiple palettes", function() {
-				var registry = new PaletteRegistry();
-				var palette1 = {name: "A"};
-				var palette2 = {name: "B"};
-				var palette3 = {name: "C"};
-				registry.add(palette1);
-				registry.add(palette2);
-				registry.add(palette3);
+      it("should replace a palette with name that has already been defined", function() {
+        var registry = new PaletteRegistry();
+        var palette1 = {name: "A"};
+        var palette2 = {name: "A"};
+        registry.add(palette1);
+        registry.add(palette2);
 
-				var palettes = registry.getAll();
-				expect(palettes instanceof Array).toBe(true);
-				expect(palettes.length).toBe(3);
-				expect(palettes.indexOf(palette1) >= 0).toBe(true);
-				expect(palettes.indexOf(palette2) >= 0).toBe(true);
-				expect(palettes.indexOf(palette3) >= 0).toBe(true);
-			});
+        var palettes = registry.getAll();
+        expect(palettes instanceof Array).toBe(true);
+        expect(palettes.length).toBe(1);
+        expect(palettes[0]).toBe(palette2);
+      });
 
-			it("should return `this`", function() {
-				var registry = new PaletteRegistry();
-				var result = registry.add({name: "A"});
+      it("should add and retain multiple palettes", function() {
+        var registry = new PaletteRegistry();
+        var palette1 = {name: "A"};
+        var palette2 = {name: "B"};
+        var palette3 = {name: "C"};
+        registry.add(palette1);
+        registry.add(palette2);
+        registry.add(palette3);
 
-				expect(result).toBe(registry);
-			});
-		});
+        var palettes = registry.getAll();
+        expect(palettes instanceof Array).toBe(true);
+        expect(palettes.length).toBe(3);
+        expect(palettes.indexOf(palette1) >= 0).toBe(true);
+        expect(palettes.indexOf(palette2) >= 0).toBe(true);
+        expect(palettes.indexOf(palette3) >= 0).toBe(true);
+      });
 
-		describe("#get(name) -", function() {
-			it("should return the palette of the given name", function() {
-				var registry = new PaletteRegistry();
-				var palette1 = {name: "A"};
-				var palette2 = {name: "B"};
-				registry.add(palette1);
-				registry.add(palette2);
+      it("should return `this`", function() {
+        var registry = new PaletteRegistry();
+        var result = registry.add({name: "A"});
 
-				expect(registry.get("B")).toBe(palette2);
-				expect(registry.get("A")).toBe(palette1);
-			});
+        expect(result).toBe(registry);
+      });
+    });
 
-			it("should return `null` when given a name that is not defined", function() {
-				var registry = new PaletteRegistry();
-				registry.add({name: "A"});
+    describe("#get(name) -", function() {
+      it("should return the palette of the given name", function() {
+        var registry = new PaletteRegistry();
+        var palette1 = {name: "A"};
+        var palette2 = {name: "B"};
+        registry.add(palette1);
+        registry.add(palette2);
 
-				expect(registry.get("B")).toBe(null);
-			});
+        expect(registry.get("B")).toBe(palette2);
+        expect(registry.get("A")).toBe(palette1);
+      });
 
-			it("should return `null` when given a falsy name, no default palette is defined and no palettes are defined", function() {
-				var registry = new PaletteRegistry();
-				expect(registry.get()).toBe(null);
-				expect(registry.get("")).toBe(null);
-				expect(registry.get(null)).toBe(null);
-				expect(registry.get(undefined)).toBe(null);
-			});
+      it("should return `null` when given a name that is not defined", function() {
+        var registry = new PaletteRegistry();
+        registry.add({name: "A"});
 
-			it("should return the first palette when given a falsy name, no default palette is defined and palettes are defined", function() {
-				var registry = new PaletteRegistry();
-				var palette1 = {name: "A"};
-				var palette2 = {name: "B"};
-				registry.add(palette1);
-				registry.add(palette2);
+        expect(registry.get("B")).toBe(null);
+      });
 
-				expect(registry.get()).toBe(palette1);
-				expect(registry.get("")).toBe(palette1);
-				expect(registry.get(null)).toBe(palette1);
-				expect(registry.get(undefined)).toBe(palette1);
-			});
+      it("should return `null` when given a falsy name, no default palette is defined and no palettes are defined", function() {
+        var registry = new PaletteRegistry();
+        expect(registry.get()).toBe(null);
+        expect(registry.get("")).toBe(null);
+        expect(registry.get(null)).toBe(null);
+        expect(registry.get(undefined)).toBe(null);
+      });
 
-			it("should return the default palette, when one is defined and was given a falsy name", function() {
-				var registry = new PaletteRegistry();
-				var palette1 = {name: "A"};
-				var palette2 = {name: "B"};
-				registry.add(palette1);
-				registry.add(palette2);
+      it("should return the first palette when given a falsy name, no default palette is defined and palettes are defined", function() {
+        var registry = new PaletteRegistry();
+        var palette1 = {name: "A"};
+        var palette2 = {name: "B"};
+        registry.add(palette1);
+        registry.add(palette2);
 
-				registry.setDefault("B");
+        expect(registry.get()).toBe(palette1);
+        expect(registry.get("")).toBe(palette1);
+        expect(registry.get(null)).toBe(palette1);
+        expect(registry.get(undefined)).toBe(palette1);
+      });
 
-				expect(registry.get()).toBe(palette2);
-				expect(registry.get("")).toBe(palette2);
-				expect(registry.get(null)).toBe(palette2);
-				expect(registry.get(undefined)).toBe(palette2);
-			});
-		});
+      it("should return the default palette, when one is defined and was given a falsy name", function() {
+        var registry = new PaletteRegistry();
+        var palette1 = {name: "A"};
+        var palette2 = {name: "B"};
+        registry.add(palette1);
+        registry.add(palette2);
 
-		describe("#getAll() -", function() {
-			it("should return color palettes in the order their names were added", function() {
-				var registry = new PaletteRegistry();
-				var palette1 = {name: "A"};
-				var palette2 = {name: "B"};
-				var palette3 = {name: "C"};
-				var palette4 = {name: "B"};
+        registry.setDefault("B");
 
-				registry.add(palette1);
-				registry.add(palette2);
-				registry.add(palette3);
-				registry.add(palette4);
+        expect(registry.get()).toBe(palette2);
+        expect(registry.get("")).toBe(palette2);
+        expect(registry.get(null)).toBe(palette2);
+        expect(registry.get(undefined)).toBe(palette2);
+      });
+    });
 
-				var palettes = registry.getAll();
-				expect(palettes instanceof Array).toBe(true);
-				expect(palettes.length).toBe(3);
-				expect(palettes[0]).toBe(palette1);
-				expect(palettes[1]).toBe(palette4);
-				expect(palettes[2]).toBe(palette3);
-			});
-		});
+    describe("#getAll() -", function() {
+      it("should return color palettes in the order their names were added", function() {
+        var registry = new PaletteRegistry();
+        var palette1 = {name: "A"};
+        var palette2 = {name: "B"};
+        var palette3 = {name: "C"};
+        var palette4 = {name: "B"};
 
-		describe("#setDefault() -", function() {
-			it("should set the default palette, returned by #get(), when given an existing name", function() {
-				var registry = new PaletteRegistry();
-				var palette1 = {name: "A"};
-				var palette2 = {name: "B"};
-				registry.add(palette1);
-				registry.add(palette2);
+        registry.add(palette1);
+        registry.add(palette2);
+        registry.add(palette3);
+        registry.add(palette4);
 
-				registry.setDefault("B");
-				expect(registry.get()).toBe(palette2);
-			});
+        var palettes = registry.getAll();
+        expect(palettes instanceof Array).toBe(true);
+        expect(palettes.length).toBe(3);
+        expect(palettes[0]).toBe(palette1);
+        expect(palettes[1]).toBe(palette4);
+        expect(palettes[2]).toBe(palette3);
+      });
+    });
 
-			it("should throw an error when given a non-existing name", function() {
-				var registry = new PaletteRegistry();
-				var palette1 = {name: "A"};
-				var palette2 = {name: "B"};
-				registry.add(palette1);
-				registry.add(palette2);
+    describe("#setDefault() -", function() {
+      it("should set the default palette, returned by #get(), when given an existing name", function() {
+        var registry = new PaletteRegistry();
+        var palette1 = {name: "A"};
+        var palette2 = {name: "B"};
+        registry.add(palette1);
+        registry.add(palette2);
 
-				expect(function() {
-					registry.setDefault("V");
-				}).toThrow();
-			});
+        registry.setDefault("B");
+        expect(registry.get()).toBe(palette2);
+      });
 
-			it("should reset the default palette, returned by #get(), when given no name or an falsy name", function() {
-				var registry = new PaletteRegistry();
-				var palette1 = {name: "A"};
-				var palette2 = {name: "B"};
-				registry.add(palette1);
-				registry.add(palette2);
+      it("should throw an error when given a non-existing name", function() {
+        var registry = new PaletteRegistry();
+        var palette1 = {name: "A"};
+        var palette2 = {name: "B"};
+        registry.add(palette1);
+        registry.add(palette2);
 
-				registry.setDefault("B");
-				expect(registry.get()).toBe(palette2);
+        expect(function() {
+          registry.setDefault("V");
+        }).toThrow();
+      });
 
-				registry.setDefault();
-				expect(registry.get()).toBe(palette1);
+      it("should reset the default palette, returned by #get(), when given no name or an falsy name", function() {
+        var registry = new PaletteRegistry();
+        var palette1 = {name: "A"};
+        var palette2 = {name: "B"};
+        registry.add(palette1);
+        registry.add(palette2);
 
-				registry.setDefault("B");
-				expect(registry.get()).toBe(palette2);
+        registry.setDefault("B");
+        expect(registry.get()).toBe(palette2);
 
-				registry.setDefault(null);
-				expect(registry.get()).toBe(palette1);
-			});
+        registry.setDefault();
+        expect(registry.get()).toBe(palette1);
 
-			it("should return `this`", function() {
-				var registry = new PaletteRegistry();
-				var result = registry.setDefault();
+        registry.setDefault("B");
+        expect(registry.get()).toBe(palette2);
 
-				expect(result).toBe(registry);
-			});
-		});
-	});
+        registry.setDefault(null);
+        expect(registry.get()).toBe(palette1);
+      });
 
-	describe("paletteRegistry singleton -", function() {
-		it("should be an instance of PaletteRegistry", function() {
-			expect(singletonPaletteRegistry instanceof PaletteRegistry).toBe(true);
-		});
+      it("should return `this`", function() {
+        var registry = new PaletteRegistry();
+        var result = registry.setDefault();
 
-		it("should be pre-loaded with eleven palettes", function() {
-			var palettes = singletonPaletteRegistry.getAll();
-			expect(palettes instanceof Array).toBe(true);
-			expect(palettes.length).toBe(11);
-		});
+        expect(result).toBe(registry);
+      });
+    });
+  });
 
-		it("should contain a palette named 'palette 1'", function() {
-			expect(singletonPaletteRegistry.get("palette 1")).toBeTruthy();
-		});
+  describe("paletteRegistry singleton -", function() {
+    it("should be an instance of PaletteRegistry", function() {
+      expect(singletonPaletteRegistry instanceof PaletteRegistry).toBe(true);
+    });
 
-		it("should contain a palette named 'palette 2'", function() {
-			expect(singletonPaletteRegistry.get("palette 2")).toBeTruthy();
-		});
+    it("should be pre-loaded with eleven palettes", function() {
 
-		it("should contain a palette named 'palette 3'", function() {
-			expect(singletonPaletteRegistry.get("palette 3")).toBeTruthy();
-		});
-	});
+      var palettes = singletonPaletteRegistry.getAll();
+      expect(palettes instanceof Array).toBe(true);
+
+      // TODO: Temporarily, 15 palettes (and not 11) due to the palettes registered by CDF.
+      expect(palettes.length).toBe(15);
+    });
+
+    it("should contain a palette named 'palette 1'", function() {
+      expect(singletonPaletteRegistry.get("palette 1")).toBeTruthy();
+    });
+
+    it("should contain a palette named 'palette 2'", function() {
+      expect(singletonPaletteRegistry.get("palette 2")).toBeTruthy();
+    });
+
+    it("should contain a palette named 'palette 3'", function() {
+      expect(singletonPaletteRegistry.get("palette 3")).toBeTruthy();
+    });
+  });
 });
