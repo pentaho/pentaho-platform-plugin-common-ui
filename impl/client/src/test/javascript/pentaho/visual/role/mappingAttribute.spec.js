66d524724ebcf402c13640dbaa56f18a1dea028f
David Santos <dsantos@pentaho.com>
2017-11-28 16:25:21 +0000

    [BACKLOG-19948] Updated unit tests

--- impl/client/src/test/javascript/pentaho/visual/role/mappingAttribute.spec.js
+++ impl/client/src/test/javascript/pentaho/visual/role/mappingAttribute.spec.js
@@ -71,23 +71,15 @@ define([
 
     describe("#keyQualitative", function() {
 
-      it("should be equal if the names are equal and all other properties different", function() {
+      it("should be equal if the names are equal", function() {
 
-        var a = new MappingAttribute({name: "a", aggregation: "sum", isReversed: true});
-        var b = new MappingAttribute({name: "a", aggregation: "avg", isReversed: false});
-
-        expect(a.keyQualitative).toBe(b.keyQualitative);
-      });
-
-      it("should be equal if the names are empty and all other properties different", function() {
-
-        var a = new MappingAttribute({aggregation: "sum", isReversed: true});
-        var b = new MappingAttribute({aggregation: "avg", isReversed: false});
+        var a = new MappingAttribute({name: "a"});
+        var b = new MappingAttribute({name: "a"});
 
         expect(a.keyQualitative).toBe(b.keyQualitative);
       });
 
-      it("should be different if the names are different and all other properties equal", function() {
+      it("should be different if the names are different", function() {
         var a = new MappingAttribute({name: "a"});
         var b = new MappingAttribute({name: "b"});
 
@@ -97,23 +89,15 @@ define([
 
     describe("#keyQuantitative", function() {
 
-      it("should be equal if the names and aggregations are equal and all other properties different", function() {
+      it("should be equal if the names are equal", function() {
 
-        var a = new MappingAttribute({name: "a", aggregation: "sum", isReversed: true});
-        var b = new MappingAttribute({name: "a", aggregation: "sum", isReversed: false});
-
-        expect(a.keyQuantitative).toBe(b.keyQuantitative);
-      });
-
-      it("should be equal if the names and aggregations are empty and all other properties different", function() {
-
-        var a = new MappingAttribute({isReversed: true});
-        var b = new MappingAttribute({isReversed: false});
+        var a = new MappingAttribute({name: "a"});
+        var b = new MappingAttribute({name: "a"});
 
         expect(a.keyQuantitative).toBe(b.keyQuantitative);
       });
 
-      it("should be different if the names are different and all other properties equal", function() {
+      it("should be different if the names are different", function() {
 
         var a = new MappingAttribute({name: "a"});
         var b = new MappingAttribute({name: "b"});
@@ -121,13 +105,6 @@ define([
         expect(a.keyQuantitative).not.toBe(b.keyQuantitative);
       });
 
-      it("should be different if the aggregations are different and all other properties equal", function() {
-
-        var a = new MappingAttribute({aggregation: "sum"});
-        var b = new MappingAttribute({aggregation: "avg"});
-
-        expect(a.keyQuantitative).not.toBe(b.keyQuantitative);
-      });
     });
 
     describe("#model and #mapping", function() {
@@ -245,15 +222,19 @@ define([
       });
 
       it("should return a string when only the name property is serialized", function() {
+        function expectIt(serializeOptions) {
+          var name = "foo";
+          var mapping = new MappingAttribute({name: name});
 
-        var name = "foo";
-        var mapping = new MappingAttribute({name: name});
+          var result = mapping.toSpecInContext(serializeOptions);
 
-        var result = mapping.toSpecInContext({includeDefaults: false});
+          specScope.dispose();
 
-        specScope.dispose();
+          expect(result).toBe(name);
+        }
 
-        expect(result).toBe(name);
+        expectIt({includeDefaults: false});
+        expectIt({includeDefaults: true});
       });
 
       it("should return an array when the base serialization is an array", function() {
@@ -265,19 +246,6 @@ define([
         specScope.dispose();
 
         expect(Array.isArray(result)).toBe(true);
-      });
-
-      it("should return a generic spec object when more than the name property is serialized", function() {
-
-        var name = "foo";
-        var mapping = new MappingAttribute({name: name});
-
-        var result = mapping.toSpecInContext({includeDefaults: true});
-
-        specScope.dispose();
-
-        expect(result instanceof Object).toBe(true);
-        expect(result.name).toBe(name);
       });
     });
   });
