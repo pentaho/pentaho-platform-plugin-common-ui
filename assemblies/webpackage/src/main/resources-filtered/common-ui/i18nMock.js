2616e9934bafc49d9133616de0a0fefa00b32fcc
Nelson Antunes <nelson.antunes@hitachivantara.com>
2018-03-20 17:30:51 +0000

    [BACKLOG-21717] Review fixes

--- assemblies/webpackage/src/main/resources-filtered/common-ui/i18nMock.js
+++ assemblies/webpackage/src/main/resources-filtered/common-ui/i18nMock.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2010 - 2016 Pentaho Corporation.  All rights reserved.
+ * Copyright 2018 Hitachi Vantara. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 define(["pentaho/i18n/MessageBundle", "json"], function(MessageBundle) {
   return {
     load: function(bundlePath, require, onLoad, config) {
