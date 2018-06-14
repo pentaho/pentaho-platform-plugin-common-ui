0e13cf7b77a1639b6430e2136a4b66222c7dad21
Duarte Cunha Leão <duarte.leao@gmail.com>
2018-06-14 20:57:32 +0100

    [CLEANUP] Add module to build the npm package used by the VizAPI tutorial

---
+++ assemblies/npmpackage/src/main/javascript/viz-api/webcontext.js
@@ -0,0 +1,33 @@
+(function(global) {
+
+  var basePath = "node_modules/@pentaho/viz-api";
+
+  global.requireCfg = {
+    paths: {},
+    shim: {},
+    map: {"*": {}},
+    bundles: {},
+    config: {
+      "pentaho/modules": {},
+      "pentaho/environment": {
+        application: "pentaho-viz-api-sandbox"
+      }
+    },
+    packages: []
+  };
+
+  global.ENVIRONMENT_CONFIG = {
+    paths: {
+      "common-ui": basePath
+    }
+  };
+
+  loadScriptSyncNested("node_modules/requirejs/require.js");
+  loadScriptSyncNested(basePath + "/common-ui-require-js-cfg.js");
+  loadScriptSyncNested(basePath + "/package-require-js-cfg.js");
+  loadScriptSyncNested(basePath + "/require-cfg.js");
+
+  function loadScriptSyncNested(src) {
+    document.write("<script language='javascript' type='text/javascript' src='" + src + "'></scr"+"ipt>");
+  }
+})(window);
