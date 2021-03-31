0e94d5c3dcfd2dfaf4b35292ae69ebb808c6aecc
davidmsantos90 <david.santos@hitachivantara.com>
2021-03-31 19:11:45 +0100

    [BACKLOG-32090] Jsdoc documentation fix

--- impl/client/src/main/javascript/web/pentaho/type/action/Change.js
+++ impl/client/src/main/javascript/web/pentaho/type/action/Change.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2010 - 2019 Hitachi Vantara. All rights reserved.
+ * Copyright 2010 - 2021 Hitachi Vantara. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ define([
 
   /**
    * @name Change
-   * @memberOf pentaho.type.action.changes
+   * @memberOf pentaho.type.action
    * @class
    * @extends pentaho.action.Base
    * @amd pentaho/type/action/Change
