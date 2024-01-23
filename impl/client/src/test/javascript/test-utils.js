d7c6aee4599a5ddc82208b8653fa8629501dc649
David Santos <david.santos@hitachivantara.com>
2024-01-23 17:49:53 +0000

    fix: jasmine unhandled promise rejection errors [BACKLOG-40011]

--- impl/client/src/test/javascript/test-utils.js
+++ impl/client/src/test/javascript/test-utils.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2010 - 2017 Hitachi Vantara.  All rights reserved.
+ * Copyright 2010 - 2024 Hitachi Vantara. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,37 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-define([
-  "pentaho/shim/es6-promise"
-], function(Promise) {
-
-  "use strict";
-
-  return {
-    expectToRejectWith: expectToRejectWith
-  };
-
-  /**
-   * Jasmine helper that _expects_ that a given async function
-   * is rejected with a given type of error.
-   *
-   * @param {function() : ?Promise} asyncTest - The async function that should throw or return a rejected promise
-   *                                with a given error type.
-   * @param {*|Error|JasmineAsymmetricEqualityTester} [error] The error that is the cause for the rejection.
-   *                                                    When unspecified, rejection of any type is still asserted.
-   *
-   * @return {Promise} The test promise.
-   */
-  function expectToRejectWith(asyncTest, error) {
-
-    return new Promise(function(resolve) { return resolve(asyncTest()); })
-        .then(function() {
-          return Promise.reject(new Error("Expected function to throw an error."));
-        }, function(ex) {
-          if(error != null) {
-            expect(ex).toEqual(error);
-          }
-          // All well, throw is expected. Swallow.
-        });
-  }
-});
+define([], () => ({
+  catchTopLevelPromise(promise) {
+    return promise.catch(() => {
+      // Prevent jasmine unhandled promise rejection error
+    });
+  },
+}));
