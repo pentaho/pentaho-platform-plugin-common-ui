1f22e0163130d75329183e08b31c373cf0ed7c6b
ssindhu23 <saisindhu.thanneeru@hitachivantara.com>
2024-11-08 09:54:16 +0000

    [BACKLOG-42753]-Upgrading pentaho-platform-plugin-common-ui from Java EE to Jakarta EE

--- impl/server/src/main/java/org/pentaho/common/ui/models/TreeBrowserModel.java
+++ impl/server/src/main/java/org/pentaho/common/ui/models/TreeBrowserModel.java
@@ -13,10 +13,10 @@
 
 package org.pentaho.common.ui.models;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
 
